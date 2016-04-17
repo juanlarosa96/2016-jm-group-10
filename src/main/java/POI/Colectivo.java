603b9edabb4e26169bcd87532c970d24098b3e0d
Marco Di Croce <marcodicro@hotmail.com>
2016-04-17 18:19:19 -0300

    agrego dependencia Joda-Time al pom y agrego clase Colectivo

---
+++ src/main/java/POI/Colectivo.java
@@ -0,0 +1,15 @@
+package POI;
+
+import org.joda.time.DateTime;
+
+public class Colectivo extends POI {
+
+	public boolean estaDisponibleEn(DateTime momento){
+		
+		//comprobar si conviene usar DateTime o Instant
+			
+		return true;
+		
+	}
+	
+}
