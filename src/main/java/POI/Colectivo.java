93bfc42f7b91f5660f5e81df6f8a12cd838076e4
Marco Di Croce <marcodicro@hotmail.com>
2016-04-17 18:46:45 -0300

    Elimino comentario sobre si usar DateTime o Instant, usaremos DateTime

--- src/main/java/POI/Colectivo.java
+++ src/main/java/POI/Colectivo.java
@@ -5,9 +5,7 @@ import org.joda.time.DateTime;
 public class Colectivo extends POI {
 
 	public boolean estaDisponibleEn(DateTime momento){
-		
-		//comprobar si conviene usar DateTime o Instant
-			
+					
 		return true;
 		
 	}
