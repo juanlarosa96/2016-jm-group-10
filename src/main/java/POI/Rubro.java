15a8494e8057232462c55c4bd6e01f318f0ac1c0
Sofia Cortes <sofiavcortes@gmail.com>
2016-04-18 17:28:31 -0300

    Constructor para rubro

--- src/main/java/POI/Rubro.java
+++ src/main/java/POI/Rubro.java
@@ -4,6 +4,13 @@ public class Rubro {
 	public String nombreRubro;
 	private Double condicionDeCercania;
 	
+	public Rubro (String nombre, Double radio)
+	{
+		this.nombreRubro = nombre;
+		this.condicionDeCercania = radio;
+	}
+	
+	
 	public Double getCondicionDeCercania() {
 		return condicionDeCercania;
 	}
