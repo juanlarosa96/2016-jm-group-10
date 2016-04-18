28bef23a0762ae05a47c34fbcf3ba2214da74e0a
Sofia Cortes <sofiavcortes@gmail.com>
2016-04-18 17:14:42 -0300

    Agregué la condición de cercanía

--- src/main/java/POI/Comercio.java
+++ src/main/java/POI/Comercio.java
@@ -8,6 +8,10 @@ public class Comercio extends POI {
 	private List<FranjaHoraria> horarios;
 	private Rubro rubro;
 	
+	public Double condicionDeCercania(){
+		return rubro.getCondicionDeCercania();
+	}
+	
 	public Boolean estaDisponible(DateTime fecha)
 	{
 		return horarios.stream().anyMatch(unHorario -> unHorario.estaEnFranjaHoraria(fecha));
