4515b45e14af68a0b73517ba853ce859a1b0dde6
Sofia Cortes <sofiavcortes@gmail.com>
2016-04-18 17:28:10 -0300

    Constructor para comercio

--- src/main/java/POI/Comercio.java
+++ src/main/java/POI/Comercio.java
@@ -1,19 +1,24 @@
 package POI;
 
 import java.util.*;
-
 import org.joda.time.DateTime;
 
 public class Comercio extends POI {
+
 	private List<FranjaHoraria> horarios;
 	private Rubro rubro;
-	
-	public Double condicionDeCercania(){
+
+	public Comercio(Rubro unRubro, List<FranjaHoraria> losHorarios) {
+		this.rubro = unRubro;
+		this.horarios = losHorarios;
+
+	}
+
+	public Double condicionDeCercania() {
 		return rubro.getCondicionDeCercania();
 	}
-	
-	public Boolean estaDisponible(DateTime fecha)
-	{
+
+	public Boolean estaDisponible(DateTime fecha) {
 		return horarios.stream().anyMatch(unHorario -> unHorario.estaEnFranjaHoraria(fecha));
 	}
 }
