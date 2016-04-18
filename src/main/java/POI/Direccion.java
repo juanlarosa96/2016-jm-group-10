7046a5763855a983f52607fa3f50eb63da0830ca
julian saidman <julian.saidman@gmail.com>
2016-04-18 15:24:28 -0300

    Constructores de colectivo, direccion y dispositivo

--- src/main/java/POI/Direccion.java
+++ src/main/java/POI/Direccion.java
@@ -5,4 +5,46 @@ public class Direccion {
 	private Integer altura;
 	private Integer piso;
 	private Character departamento;
+	
+	public Direccion(String unaCalle, Integer unaAltura, Integer unPiso, Character unDepartamento ){
+		this.calle = unaCalle;
+		this.altura = unaAltura;
+		this.piso = unPiso;
+		this.departamento = unDepartamento;
+		
+		
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public Integer getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Integer altura) {
+		this.altura = altura;
+	}
+
+	public Integer getPiso() {
+		return piso;
+	}
+
+	public void setPiso(Integer piso) {
+		this.piso = piso;
+	}
+
+	public Character getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Character departamento) {
+		this.departamento = departamento;
+	}
+	
 }
--- src/main/java/POI/Dispositivo.java
+++ src/main/java/POI/Dispositivo.java
@@ -5,7 +5,14 @@ import org.uqbar.geodds.Point;
 
 public class Dispositivo {
 	private Point posicion;
-	private List<POI> pois;
+	private List<POI> listaPois;
+	
+	public Dispositivo(Point unaPosicion, List<POI> unosPois){
+		this.posicion = unaPosicion;
+		this.listaPois = unosPois;
+		
+		
+	}
 
 	public Double distanciaEntrePois(POI unPoi, POI otroPoi) {
 
