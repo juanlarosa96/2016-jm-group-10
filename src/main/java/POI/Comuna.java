b05aa1f907460ecdeaba2caf21f6483ef57640ce
Marco Di Croce <marcodicro@hotmail.com>
2016-04-18 15:44:52 -0300

    subo el constructor de la comuna

--- src/main/java/POI/Comuna.java
+++ src/main/java/POI/Comuna.java
@@ -1,13 +1,21 @@
 package POI;
 
+import java.util.List;
+
 import org.uqbar.geodds.Point;
 import org.uqbar.geodds.Polygon;
 
 public class Comuna {
-int numero;
-Polygon territorio;
-	
-	public Boolean incluyeA(Point unaPosicion){
+	int numero;
+	Polygon territorio;
+
+	public Comuna(int unNumero, List<Point> puntosFrontera) {
+		numero = unNumero;
+		puntosFrontera.stream().map(punto -> territorio.add(punto));
+	}
+
+	public Boolean incluyeA(Point unaPosicion) {
 		return territorio.isInside(unaPosicion);
 	}
+
 }
