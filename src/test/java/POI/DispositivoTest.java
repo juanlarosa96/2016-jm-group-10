e9af4406e42a1f5e8648ec4f889fcd96cebce175
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-04-19 15:49:17 -0300

    init de DispositivoTest completo

--- src/test/java/POI/DispositivoTest.java
+++ src/test/java/POI/DispositivoTest.java
@@ -11,56 +11,59 @@ import org.uqbar.geodds.Point;
 public class DispositivoTest {
 
 	private Dispositivo dispositivo;
+	private Point posicionDispositivo;
 
 	private CGP cgpComuna10;
-	private Banco bancoProvincia;
-	private Comercio elHalcon;
-	private Comuna comuna10;
-	private ParadaColectivo parada114;
-
-	private Rubro restaurant;
+	private Direccion direccionCgpComuna10;
+	private Point posicionCgpComuna10;
+	private List<Servicio> serviciosCGP;
+	private List<String> etiquetasCGP;
+	
 	private Servicio rentas;
 	private Servicio asesoramientoJuridico;
 	private Servicio ecobici;
-
-	private Direccion direccionCgpComuna10;
-	private Direccion direccionElHalcon;
-	private Direccion direccionBancoProvincia;
-	private Direccion direccionParada114;
-
-	private Point posicionDispositivo;
-	private Point posicionCgpComuna10;
-	private Point posicionElHalcon;
-	private Point posicionBancoProvincia;
-	private Point posicionParada114;
-
-	private List<FranjaHoraria> horariosBanco;
-	private List<String> etiquetasBancoProvincia;
-	private List<Servicio> serviciosCGP;
-	private List<String> etiquetasCGP;
+	
+	private Comuna comuna10;
 	private List<Point> limitesComuna10;
+	
+	private Banco bancoProvincia;
+	private Direccion direccionBancoProvincia;
+	private Point posicionBancoProvincia;
+	private List<String> etiquetasBancoProvincia;
+	
+	private Comercio elHalcon;
+	private Direccion direccionElHalcon;
+	private Point posicionElHalcon;
+	private Rubro restaurant;
+	private List<FranjaHoraria> horariosElHalcon;
+	private List<String> etiquetasElHalcon;
+	
+	private ParadaColectivo parada114;	
+	private Direccion direccionParada114;	
+	private Point posicionParada114;
+	private List<String> etiquetasParada114;
 
 	@Before
 	public void init() {
 		posicionDispositivo = new Point(-34.631402, -58.488060);
-		posicionCgpComuna10 = new Point(-34.6369004, -58.4959096);
-		posicionElHalcon = new Point(-34.6327106, -58.4877209);
-		posicionBancoProvincia = new Point(-34.6327475, -58.4851585);
-		posicionParada114 = new Point(-34.631997, -58.484737);
-		
-		direccionCgpComuna10 = new Direccion("Bacacay", 3968, "Campana", "Concordia", null, null, 1407, "CABA",
-				"Floresta", "CABA", "Argentina");
-		direccionElHalcon = new Direccion("Av. Rivadavia", 8451, "Mercedes", "Av. Segurola", null, null, 1407, "CABA",
-				"Floresta", "CABA", "Argentina");
-		direccionBancoProvincia = new Direccion("Av. Rivadavia", 8468, "Benedetti", "Mariano Acosta", null, null, 1407,
-				"CABA", "Floresta", "CABA", "Argentina");
-		direccionParada114 = new Direccion("Av. Segurola", 230, "Bacacay", "Bogota", null, null, 1407, "CABA",
-				"Floresta", "CABA", "Argentina");
-		
 		dispositivo = new Dispositivo(posicionDispositivo);
 		
-		bancoProvincia = new Banco(posicionBancoProvincia, "Banco Provincia", direccionBancoProvincia,
-				etiquetasBancoProvincia);
+		posicionParada114 = new Point(-34.631997, -58.484737);
+		direccionParada114 = new Direccion("Av. Segurola", 230, "Bacacay", "Bogota", null, null, 1407, "CABA",
+				"Floresta", "CABA", "Argentina");
+		etiquetasParada114 = new ArrayList<String>(){
+			{
+				add("Parada");
+				add("parada");
+				add("114");
+				add("colectivo");
+			}
+		};
+		parada114 = new ParadaColectivo(114, posicionParada114, "114", direccionParada114, etiquetasParada114);
+
+		posicionBancoProvincia = new Point(-34.6327475, -58.4851585);
+		direccionBancoProvincia = new Direccion("Av. Rivadavia", 8468, "Benedetti", "Mariano Acosta", null, null, 1407,
+				"CABA", "Floresta", "CABA", "Argentina");
 		etiquetasBancoProvincia = new ArrayList<String>() {
 			{
 				add("banco");
@@ -70,8 +73,12 @@ public class DispositivoTest {
 				add("cajero");
 			}
 		};
-
-		cgpComuna10 = new CGP(serviciosCGP, comuna10, posicionCgpComuna10, "CGP Comuna 10", direccionCgpComuna10, etiquetasCGP);
+		bancoProvincia = new Banco(posicionBancoProvincia, "Banco Provincia", direccionBancoProvincia,
+				etiquetasBancoProvincia);
+		
+		posicionCgpComuna10 = new Point(-34.6369004, -58.4959096);
+		direccionCgpComuna10 = new Direccion("Bacacay", 3968, "Campana", "Concordia", null, null, 1407, "CABA",
+				"Floresta", "CABA", "Argentina");
 		serviciosCGP = new ArrayList<Servicio>() {
 			{
 				add(rentas);
@@ -90,8 +97,9 @@ public class DispositivoTest {
 				add("ecobici");
 			}
 		};
-	
-		comuna10 = new Comuna(10, limitesComuna10);
+		cgpComuna10 = new CGP(serviciosCGP, comuna10, posicionCgpComuna10, "CGP Comuna 10", direccionCgpComuna10,
+				etiquetasCGP);
+		
 		limitesComuna10 = new ArrayList<Point>() {
 			{
 				add(new Point(-34.611015, -58.529025));
@@ -113,6 +121,44 @@ public class DispositivoTest {
 				add(new Point(-34.620399, -58.516870));
 			}
 		};
+		comuna10 = new Comuna(10, limitesComuna10);
+		
+		posicionElHalcon = new Point(-34.6327106, -58.4877209);
+		direccionElHalcon = new Direccion("Av. Rivadavia", 8451, "Mercedes", "Av. Segurola", null, null, 1407, "CABA",
+				"Floresta", "CABA", "Argentina");
+		horariosElHalcon = new ArrayList<FranjaHoraria>() {
+			{
+				add(new FranjaHoraria(1, new LocalTime(9,30), new LocalTime(13,0)));
+				add(new FranjaHoraria(1, new LocalTime(14,30), new LocalTime(19,0)));
+				add(new FranjaHoraria(2, new LocalTime(9,30), new LocalTime(13,0)));
+				add(new FranjaHoraria(2, new LocalTime(14,30), new LocalTime(19,0)));
+				add(new FranjaHoraria(3, new LocalTime(9,30), new LocalTime(13,0)));
+				add(new FranjaHoraria(3, new LocalTime(14,30), new LocalTime(19,0)));
+				add(new FranjaHoraria(4, new LocalTime(9,30), new LocalTime(13,0)));
+				add(new FranjaHoraria(4, new LocalTime(14,30), new LocalTime(19,0)));
+				add(new FranjaHoraria(5, new LocalTime(9,30), new LocalTime(13,0)));
+				add(new FranjaHoraria(5, new LocalTime(14,30), new LocalTime(19,0)));
+				add(new FranjaHoraria(6, new LocalTime(10,0), new LocalTime(14,0)));
+				
+			}
+		};
+		etiquetasElHalcon = new ArrayList<String>() {
+			{
+				add("restaurant");
+				add("almuerzo");
+				add("cena");
+				add("comida");
+				add("el");
+				add("halcon");
+				add("efectivo");
+				add("tarjeta");
+				add("credito");
+				add("debito");
+			}
+		};
+		elHalcon = new Comercio(restaurant, horariosElHalcon, posicionElHalcon, "El Halcon", direccionElHalcon, etiquetasElHalcon);
+	
+		
 	}
 
 }
