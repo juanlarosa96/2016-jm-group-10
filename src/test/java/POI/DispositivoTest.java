9a1bf0d3b00d696ee1f7892275aadc710139f41c
Sofía Cortés <sofiavcortes@gmail.com>
2016-05-11 13:11:51 -0300

    Agrego CGP Valido en DispositivoTest

--- src/test/java/POI/DispositivoTest.java
+++ src/test/java/POI/DispositivoTest.java
@@ -15,19 +15,12 @@ public class DispositivoTest {
 
 	private Dispositivo dispositivo;
 	private Point posicionDispositivo;
+	
+	private CGP cgpValido;
 
-	private CGP cgpComuna10;
-	private Direccion direccionCgpComuna10;
-	private Point posicionCgpComuna10;
-	private List<Servicio> serviciosCGP10;
-	private List<String> etiquetasCGP10;
 
 	private Servicio rentas;
-	private Servicio asesoramientoJuridico;
 	private Servicio ecobici;
-
-	private Comuna comuna10;
-	private List<Point> limitesComuna10;
 
 	private Banco bancoValido;
 	
@@ -49,14 +42,12 @@ public class DispositivoTest {
 	private List<POI> listaPoisDispositivo;
 	private DateTime lunes4abril10am;
 	private DateTime martes5abril2am;
-	private List<FranjaHoraria> horariosRentas;
-	private List<FranjaHoraria> horariosAsesoramientoJuridico;
-	private List<FranjaHoraria> horariosEcobici;
+
 	private Point posicionMcDonalds;
 	private Direccion direccionMcDonalds;
 	private ArrayList<String> etiquetasMcDonalds;
 	private Comercio mcDonalds;
-	private ArrayList<FranjaHoraria> horariosAsesoramientoLegal;
+
 	private Servicio asesoramientoLegal;
 	private ArrayList<Point> limitesComuna5;
 	private Direccion direccionCgpComuna5;
@@ -100,93 +91,7 @@ public class DispositivoTest {
 
 		bancoValido = FixtureBanco.dameUnBancoValido();
 
-		posicionCgpComuna10 = new Point(-34.6369004, -58.4959096);
-		direccionCgpComuna10 = new Direccion("Bacacay", 3968, "Campana", "Concordia", null, null, 1407, "CABA",
-				"Floresta", "CABA", "Argentina");
-
-		horariosRentas = new ArrayList<FranjaHoraria>() {
-			{
-				add(new FranjaHoraria(1, new LocalTime(9, 30), new LocalTime(14, 0)));
-				add(new FranjaHoraria(2, new LocalTime(9, 30), new LocalTime(14, 0)));
-				add(new FranjaHoraria(3, new LocalTime(9, 30), new LocalTime(14, 0)));
-				add(new FranjaHoraria(4, new LocalTime(9, 30), new LocalTime(14, 0)));
-				add(new FranjaHoraria(5, new LocalTime(9, 30), new LocalTime(14, 0)));
-			}
-		};
-
-		horariosAsesoramientoJuridico = new ArrayList<FranjaHoraria>() {
-			{
-				add(new FranjaHoraria(2, new LocalTime(9, 0), new LocalTime(16, 0)));
-			}
-		};
-
-		horariosEcobici = new ArrayList<FranjaHoraria>() {
-			{
-				add(new FranjaHoraria(1, new LocalTime(10, 0), new LocalTime(15, 0)));
-				add(new FranjaHoraria(2, new LocalTime(10, 0), new LocalTime(15, 0)));
-				add(new FranjaHoraria(3, new LocalTime(10, 0), new LocalTime(15, 0)));
-				add(new FranjaHoraria(4, new LocalTime(10, 0), new LocalTime(15, 0)));
-				add(new FranjaHoraria(5, new LocalTime(10, 0), new LocalTime(15, 0)));
-			}
-		};
-
-		horariosAsesoramientoLegal = new ArrayList<FranjaHoraria>() {
-			{
-				add(new FranjaHoraria(1, new LocalTime(10, 0), new LocalTime(17, 0)));
-				add(new FranjaHoraria(3, new LocalTime(10, 0), new LocalTime(17, 0)));
-			}
-		};
-
-		rentas = new Servicio("Rentas", horariosRentas);
-		asesoramientoJuridico = new Servicio("asesoramiento juridico", horariosAsesoramientoJuridico);
-		ecobici = new Servicio("Ecobici", horariosEcobici);
-		asesoramientoLegal = new Servicio("asesoramiento legal", horariosAsesoramientoLegal);
-
-		serviciosCGP10 = new ArrayList<Servicio>() {
-			{
-				add(rentas);
-				add(asesoramientoJuridico);
-				add(ecobici);
-			}
-		};
-		etiquetasCGP10 = new ArrayList<String>() {
-			{
-				add("CGP");
-				add("Cgp");
-				add("cgp");
-				add("rentas");
-				add("asesoramiento");
-				add("juridico");
-				add("ecobici");
-			}
-		};
-
-		limitesComuna10 = new ArrayList<Point>() {
-			{
-				add(new Point(-34.611015, -58.529025));
-				add(new Point(-34.615256, -58.531221));
-				add(new Point(-34.634217, -58.529806));
-				add(new Point(-34.634495, -58.510883));
-				add(new Point(-34.639959, -58.509675));
-				add(new Point(-34.645743, -58.502325));
-				add(new Point(-34.643387, -58.497065));
-				add(new Point(-34.644764, -58.495284));
-				add(new Point(-34.636925, -58.478568));
-				add(new Point(-34.638797, -58.476058));
-				add(new Point(-34.636537, -58.471423));
-				add(new Point(-34.622482, -58.477860));
-				add(new Point(-34.624637, -58.482710));
-				add(new Point(-34.614536, -58.495627));
-				add(new Point(-34.609238, -58.500219));
-				add(new Point(-34.616726, -58.513008));
-				add(new Point(-34.620399, -58.516870));
-			}
-		};
-
-		comuna10 = new Comuna(10, limitesComuna10);
-
-		cgpComuna10 = new CGP(serviciosCGP10, comuna10, posicionCgpComuna10, "CGP Comuna 10", direccionCgpComuna10,
-				etiquetasCGP10);
+		cgpValido = FixtureCGP.dameCGPValido();
 
 		posicionCgpComuna5 = new Point(-34.6229418, -58.4146764);
 		direccionCgpComuna5 = new Direccion("Carlos Calvo", 3307, "Virrey Liniers", "Sanchez de Loria", null, null,
@@ -287,7 +192,7 @@ public class DispositivoTest {
 				add(elHalcon);
 				add(mcDonalds);
 				add(bancoValido);
-				add(cgpComuna10);
+				add(cgpValido);
 				add(cgpComuna5);
 			}
 		};
@@ -321,7 +226,7 @@ public class DispositivoTest {
 	public void consultoSiUnServicioValidoSeEncuentraDisponibleEnHorarioAbiertoEnAlgunCGPYDiceQueSeEncuentraEn2CGP() {
 		CGPsConRentas = dispositivo.buscarServicioDisponible("Rentas", lunes4abril10am);
 		Assert.assertEquals(2, CGPsConRentas.size(), 0);
-		Assert.assertTrue(CGPsConRentas.contains(cgpComuna10));
+		Assert.assertTrue(CGPsConRentas.contains(cgpValido));
 		Assert.assertTrue(CGPsConRentas.contains(cgpComuna5));
 	}
 
