f66ffb50ed8af1e4cf0451946d882959bf7a41c4
Juan Larosa <juanlarosa96@hotmail.com>
2016-05-17 20:03:52 -0300

    Modifico metodo buscar en ServicioExternoBancos para que retorne un String en vez de un HashMap

--- src/main/java/POI/ServicioExternoBancos.java
+++ src/main/java/POI/ServicioExternoBancos.java
@@ -1,9 +1,7 @@
 package POI;
 
-import java.util.HashMap;
-
 public interface ServicioExternoBancos {
 
-	HashMap<String,String> buscar(String banco, String servicio);
+	String buscar(String banco, String servicio);
 
 }
