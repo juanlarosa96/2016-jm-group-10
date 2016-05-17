16f66419ded7b78487c8b128128b49673ccecdb9
Juan Larosa <juanlarosa96@hotmail.com>
2016-05-17 20:02:38 -0300

    Agrego metodo de adaptarBancos en BancoAdapter

--- src/main/java/POI/BancoAdapter.java
+++ src/main/java/POI/BancoAdapter.java
@@ -1,8 +1,12 @@
 package POI;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.uqbar.geodds.Point;
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
 
 
 public class BancoAdapter implements ComponenteExternoAdapter {
@@ -15,14 +19,35 @@ public class BancoAdapter implements ComponenteExternoAdapter {
 		
 		String[] parametros = descripcion.split(",", 2);
 		
-		return this.adaptarBancos(servicioExternoBancos.buscar(parametros[0],parametros[1]));
-		
-		
+		return this.adaptarBancos(servicioExternoBancos.buscar(parametros[0],parametros[1]));		
 	}
 
-	private ArrayList<POI> adaptarBancos(HashMap<String, String> hashMap) {
+	private ArrayList<POI> adaptarBancos(String json) {
 		// aca adaptar con el parser
-		return null;
+		
+		ArrayList<BancoJson> listaJson = new ArrayList<BancoJson>();
+		ArrayList<POI> listaBancos;
+		Type tipoListaBancosJson = new TypeToken<Collection<POI>>(){}.getType();
+		Gson parserGson = new Gson();
+		
+		listaJson = parserGson.fromJson(json,tipoListaBancosJson);
+		
+		listaBancos = (ArrayList<POI>) listaJson.stream().map(bancoJson-> convertirABanco(bancoJson)).collect(Collectors.toList());
+		
+		
+		return listaBancos;
+	}
+
+	private POI convertirABanco(BancoJson bancoJson) {
+		
+		String nombre = bancoJson.getBanco();
+		Point point = new Point(bancoJson.getX(),bancoJson.getY());
+		Direccion direccion = null;		
+		ArrayList <String> etiquetas = bancoJson.getServicios();
+		
+		 
+		return new Banco(point,nombre,direccion, etiquetas);
+		
 	}
 
 }
