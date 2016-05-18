4fec02a5c417d42376a77043dce61623efbf896f
Marco Di Croce <marcodicro@hotmail.com>
2016-05-18 17:44:00 -0300

    agrego test para agregar poi que no esta en la lista

--- src/test/java/tests/DispositivoTest.java
+++ src/test/java/tests/DispositivoTest.java
@@ -40,6 +40,7 @@ public class DispositivoTest {
 	private DateTime horarioValidoParaRentas;
 	private DateTime horarioNoValidoParaNingunServicio;
 	private List<POI> CGPsConRentas;
+	private ParadaColectivo paradaQueNoEstaEnLaLista;
 
 	@Before
 	public void init() {
@@ -58,7 +59,7 @@ public class DispositivoTest {
 		otroCgpValido = FixtureCGP.dameOtroCgpValido();
 
 		comercioValido = FixtureComercio.dameComercioValido();
-
+		
 		listaPoisDispositivo = new ArrayList<POI>() {
 			{
 				add(parada114Valida);
@@ -73,6 +74,8 @@ public class DispositivoTest {
 		Dispositivo.setListaPois(listaPoisDispositivo);
 
 		CGPsConRentas = new ArrayList<POI>();
+		
+		paradaQueNoEstaEnLaLista= FixtureParadaColectivo.dameUnaTercerParadaValida();
 	}
 
 	@Test
@@ -114,6 +117,14 @@ public class DispositivoTest {
 		CGPsConRentas = dispositivo.buscarServicioDisponible("Rentas", horarioNoValidoParaNingunServicio);
 		Assert.assertEquals(0, CGPsConRentas.size(), 0);
 
+	}
+	
+	@Test
+	public void SiAgregoUnaParadaQueNoEstaEnLaListaLaAgrega(){
+		Assert.assertFalse(listaPoisDispositivo.contains(paradaQueNoEstaEnLaLista));
+		Dispositivo.agregarPoi(paradaQueNoEstaEnLaLista);
+		Assert.assertTrue(listaPoisDispositivo.contains(paradaQueNoEstaEnLaLista));
+		
 	}
 
 }
