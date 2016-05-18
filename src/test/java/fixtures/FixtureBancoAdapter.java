ae6dc0338a68bd5979f96a201da6a9f03ce1416a
Sofía Cortés <sofiavcortes@gmail.com>
2016-05-18 18:54:08 -0300

    Agrego fixture para BancoAdapter

---
+++ src/test/java/fixtures/FixtureBancoAdapter.java
@@ -0,0 +1,63 @@
+package fixtures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uqbar.geodds.Point;
+
+import com.google.gson.Gson;
+
+import tpaPOIs.Banco;
+import tpaPOIs.BancoJson;
+import tpaPOIs.Direccion;
+
+public class FixtureBancoAdapter {
+
+	private static BancoJson unBancoJson;
+
+	private static void inicializarBancoJson() {
+		
+		ArrayList<String> listaServicios = new ArrayList<String>() {
+			{
+				add("cobro cheques");
+				add("depositos");
+				add("extracciones");
+				add("transferencias");
+				add("creditos");
+				add("");
+				add("");
+				add("");
+			}
+		};
+
+		unBancoJson = new BancoJson("Banco de la Plaza", -35.9338322, 72.348353, "Avellaneda", "Javier Loeschbor",
+				listaServicios);
+		
+		return;
+		
+	}
+	
+	public static String devolverListaBancoJsonNoVacia() {
+
+		FixtureBancoAdapter.inicializarBancoJson();		
+		List<BancoJson> listaBancoJson = new ArrayList<BancoJson>();
+
+		listaBancoJson.add(unBancoJson);
+
+		return new Gson().toJson(listaBancoJson);
+
+	}
+
+	public static Banco devolverBancoValido() {
+		
+		FixtureBancoAdapter.inicializarBancoJson();
+		String nombre = unBancoJson.getBanco();
+		Point point = new Point(unBancoJson.getX(), unBancoJson.getY());
+		Direccion direccion = null;
+		ArrayList<String> etiquetas = unBancoJson.getServicios();
+
+		return new Banco(point, nombre, direccion, etiquetas);
+
+	}
+
+}
