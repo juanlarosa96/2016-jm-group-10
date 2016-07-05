e272540b31d2fc578f9e251822d7b07915063788
Marco Di Croce <marcodicro@hotmail.com>
2016-07-05 20:07:13 -0300

    agrego metodo enviarMailPorErrorEnProceso a interfaz AdapterMail

--- src/main/java/adapters/AdapterMail.java
+++ src/main/java/adapters/AdapterMail.java
@@ -1,9 +1,12 @@
 package adapters;
 
 import eventosBusqueda.Busqueda;
+import procesos.Proceso;
 
 public interface AdapterMail {
 
 	public void enviarMailPorBusquedaLenta(Busqueda unaBusqueda, String direccionEmailDestino);
+
+	public void enviarMailPorErrorDeProceso(Proceso proceso, String direccionEmailDestino);
 
 }
