e17889d0487a8e3c88c71b6336e6ace1aa380747
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-07-05 17:25:51 -0300

    Creo constructor de la clase Proceso, modifico los parametros que recibe el metodo configurarProcesos, ahora recibe accion, frecuencia y fecha

--- src/main/java/procesos/ManejadorDeProcesos.java
+++ src/main/java/procesos/ManejadorDeProcesos.java
@@ -1,10 +1,15 @@
 package procesos;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.SortedSet;
+
 import org.joda.time.DateTime;
 
-public class ManejadorDeProcesos {
+public class ManejadorDeProcesos implements Runnable {
 	
 	private static ManejadorDeProcesos singleton;
+	private List<Proceso> procesos;
 
 	private ManejadorDeProcesos(){
 		
@@ -19,8 +24,20 @@ public class ManejadorDeProcesos {
 	}
 	
 	
-	public void configurarProceso(Proceso proceso,DateTime fechaYhoraDeEjecucion){
-		
+	public void configurarProceso(Accion accion, Double frecuencia,DateTime fechaYhoraDeEjecucion){
+		//Agregar a una lista ordenada por horario de procesos
+		procesos.add(new Proceso(accion, frecuencia, fechaYhoraDeEjecucion));
 	}
 	
+	@Override
+	public void run() {
+		//Verificar el horario y ejecutar el proceso si corresponde
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
 }
+
--- src/main/java/procesos/Proceso.java
+++ src/main/java/procesos/Proceso.java
@@ -1,9 +1,35 @@
 package procesos;
 
+import org.joda.time.DateTime;
+
 public class Proceso {
 
 	private Accion accion;
 	private Double frecuenciaEnHoras;
+	private DateTime fechaYhoraDeEjecucion;
+	
+	public Proceso(Accion accion, Double frecuencia, DateTime fechaYhora){
+		this.accion = accion;
+		this.frecuenciaEnHoras = frecuencia;
+		this.fechaYhoraDeEjecucion = fechaYhora;
+	}
+	
+	
+	public Accion getAccion() {
+		return accion;
+	}
+
+
+	public DateTime getFechaYhoraDeEjecucion() {
+		return fechaYhoraDeEjecucion;
+	}
+
+
+	public Double getFrecuenciaEnHoras() {
+		return frecuenciaEnHoras;
+	}
+	
+	
 	//o podrian ser dos clases que hereden de proceso (abstracta), una con frecuencia y otra sin. 
 	
 }
