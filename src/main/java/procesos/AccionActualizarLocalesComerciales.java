3aea1912dd4234c744b568c37bd044544282ed57
Marco Di Croce <marcodicro@hotmail.com>
2016-07-05 20:12:32 -0300

    ejecutar devuelve resultado ejecucion

--- src/main/java/procesos/AccionActualizarLocalesComerciales.java
+++ src/main/java/procesos/AccionActualizarLocalesComerciales.java
@@ -3,33 +3,42 @@ package procesos;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
+
+import org.joda.time.DateTime;
+
 import pois.ManejadorDePois;
 
 public class AccionActualizarLocalesComerciales implements Accion {
 
 	private String rutaArchivo;
 	private ManejadorDePois manejedorPois;
-	
+
 	@Override
-	public void ejecutar() throws Exception {
+	public ResultadoEjecucion ejecutar() throws Exception {
 
 		String linea;
 		BufferedReader lectorArchivo = null;
 
 		try {
 			lectorArchivo = new BufferedReader(new FileReader(rutaArchivo));
+
+			Integer cantElementosAfectados = 0;
+
 			while ((linea = lectorArchivo.readLine()) != null) {
 				String[] lineaComercio = linea.split(";", 2);
 				String[] etiquetas = lineaComercio[1].split(" ");
-				manejedorPois.actualizarEtiquetasLocalesComerciales(lineaComercio[0], Arrays.asList(etiquetas));
+				cantElementosAfectados += manejedorPois.actualizarEtiquetasLocalesComerciales(lineaComercio[0],
+						Arrays.asList(etiquetas));
 			}
 
+			lectorArchivo.close();
+			
+			return new ResultadoEjecucion(cantElementosAfectados, DateTime.now(), "Locales Comerciales Actualizados");
+
+		} catch (Exception e) {
+			throw new Exception();
 		}
-		catch(Exception e){
-				throw new Exception();		
-		}
-		
-		lectorArchivo.close();
+
 	}
 
 	public AccionActualizarLocalesComerciales(String ruta) {
