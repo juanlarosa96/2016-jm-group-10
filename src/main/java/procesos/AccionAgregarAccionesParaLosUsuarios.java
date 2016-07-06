8f8ce5bad1fc3efd79d6a46b23495176713a79db
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-07-06 20:59:47 -0300

    Arreglo error en ProcesoTest y agrego clase AccionAgregarAccionesParaLosUsuarios que se habia borrado

---
+++ src/main/java/procesos/AccionAgregarAccionesParaLosUsuarios.java
@@ -0,0 +1,31 @@
+package procesos;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import eventosBusqueda.InteresadoEnBusquedas;
+import pois.Dispositivo;
+
+public class AccionAgregarAccionesParaLosUsuarios implements Accion {
+
+	private InteresadoEnBusquedas accionUsuario;
+	private CriterioSeleccionUsuarios criterio;
+	
+	public AccionAgregarAccionesParaLosUsuarios(InteresadoEnBusquedas accion, CriterioSeleccionUsuarios criterio) {
+		this.accionUsuario = accion;
+		this.criterio = criterio;
+	}
+
+	@Override
+	public ResultadoEjecucion ejecutar() throws ExceptionErrorEjecucionDeAccion {
+		try{
+			List<Dispositivo> usuarios = criterio.filtrar();
+			usuarios.stream().forEach(usuario -> usuario.agregarInteresadoEnBusquedas(accionUsuario));
+			return new ResultadoEjecucion(usuarios.size(),DateTime.now(),"Acciones agregadas para los usuarios seleccionados");
+		}
+		catch(Exception e){
+			throw new ExceptionErrorEjecucionDeAccion();
+		}	
+	}
+}
