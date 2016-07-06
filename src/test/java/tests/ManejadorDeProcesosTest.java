aad8e0648f2851173a8434a774cc4f7303292b7f
JulianIP <jipandullo@gmail.com>
2016-07-06 18:49:29 -0300

    Agrego test de ManejadorDeProcesos

--- src/test/java/tests/ManejadorDeProcesosTest.java
+++ src/test/java/tests/ManejadorDeProcesosTest.java
@@ -23,6 +23,7 @@ import org.mockito.Mockito;
 import procesos.Accion;
 import procesos.CriterioDeManejoDeError;
 import procesos.CriterioSinManejoDeError;
+import procesos.ExceptionErrorEjecucionDeAccion;
 import procesos.ManejadorDeProcesos;
 import procesos.Proceso;
 import procesos.ResultadoEjecucion;;
@@ -32,16 +33,19 @@ public class ManejadorDeProcesosTest {
 	private Accion otraAccionValida;
 	private ManejadorDeProcesos manejadorProcesos;
 	private ResultadoEjecucion resultadoEjecucion;
+	private Accion accionEsperarUnSegundo;
 
 	@Before
 	public void init() {
 		resultadoEjecucion = new ResultadoEjecucion(0, DateTime.now(), null);
 		accionValida = mock(Accion.class);
 		otraAccionValida = mock(Accion.class);
+		accionEsperarUnSegundo = mock(Accion.class);
 		manejadorProcesos = ManejadorDeProcesos.getInstance();
 		manejadorProcesos.vaciarListaProcesosEjecutados();
-
+		
 		try {
+			when(accionEsperarUnSegundo.ejecutar());
 			when(accionValida.ejecutar()).thenReturn(resultadoEjecucion);
 			when(otraAccionValida.ejecutar()).thenReturn(resultadoEjecucion);
 		} catch (Exception e) {
@@ -105,7 +109,7 @@ public class ManejadorDeProcesosTest {
 
 		DateTime tiempoEjecucionProcesoEnLista = DateTime.now().plusMillis(1000);
 		DateTime tiempoEjecucionProcesoEntrante = DateTime.now().plusMillis(1000);
-
+	
 		manejadorProcesos.configurarProceso(accionValida, 0.0, tiempoEjecucionProcesoEnLista, null);
 		manejadorProcesos.configurarProceso(otraAccionValida, 0.0, tiempoEjecucionProcesoEntrante, null);
 
@@ -118,6 +122,30 @@ public class ManejadorDeProcesosTest {
 		Assert.assertTrue(listaProcesosEjecutados.get(0).getAccion() == accionValida);
 		Assert.assertTrue(listaProcesosEjecutados.get(1).getAccion() == otraAccionValida);
 
+	}
+	@Test
+	public void testSiSeConfiguraUnProcesoParaUnMomentoEnElQueHayOtroEjecutandoEsperaHastaQueTermine(){
+		
+		DateTime tiempoEjecucionProceso1 = DateTime.now();
+		DateTime tiempoEjecucionProceso2 = DateTime.now().plusMillis(500);
+		
+		manejadorProcesos.configurarProceso(accionEsperarUnSegundo, 0.0, tiempoEjecucionProceso1, null);
+		manejadorProcesos.configurarProceso(accionValida, 0.0, tiempoEjecucionProceso2, null);
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+		}
+		List<Proceso> listaProcesosEjecutados = manejadorProcesos.getProcesosEjecutados();
+
+		Assert.assertTrue(listaProcesosEjecutados.get(0).getAccion() == accionEsperarUnSegundo);
+		Assert.assertEquals(1,listaProcesosEjecutados.size(),0);
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		Assert.assertTrue(listaProcesosEjecutados.get(1).getAccion() == accionValida);
 	}
 
 }
