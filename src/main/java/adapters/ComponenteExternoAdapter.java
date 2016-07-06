2bbde3a3c6ffca6a1012bc8dfc344af9473a8c21
Marco Di Croce <marcodicro@hotmail.com>
2016-07-06 16:12:57 -0300

    agrego buscarPoisDadosDeBaja a la interfaz ComponenteExternoAdapter

--- src/main/java/adapters/ComponenteExternoAdapter.java
+++ src/main/java/adapters/ComponenteExternoAdapter.java
@@ -1,11 +1,14 @@
 package adapters;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import pois.POI;
 
 public interface ComponenteExternoAdapter {
 
 	public ArrayList<POI> buscarPoisExternos(String descripcion);
+
+	public List<POI> buscarPoisDadosDeBaja(String urlPoisAEliminar);
 
 }
