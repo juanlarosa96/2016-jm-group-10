5fd41bccf7ada629a4f4144a4733e3629bf1adc2
Sofia Cortes <sofiavcortes@gmail.com>
2016-07-06 19:04:36 -0300

    Agrego test  de caso feliz en criterioComunaTest

--- src/test/java/tests/CriterioComunaTest.java
+++ src/test/java/tests/CriterioComunaTest.java
@@ -2,9 +2,13 @@ package tests;
 
 import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.uqbar.geodds.Point;
 
+import adapters.CgpAdapter;
+import adapters.ServicioExternoCGP;
 import pois.Dispositivo;
 import pois.ManejadorDeDispositivos;
 import procesos.CriterioComuna;
@@ -19,12 +23,16 @@ public class CriterioComunaTest {
 	private ArrayList<Dispositivo> dispositivosValidos;
 	private ManejadorDeDispositivos manejadorDeDispositivos;
 	private CriterioComuna criterioComuna;
+	private CgpAdapter cgpAdapter;
+	private ServicioExternoCGP servicioExternoCGP;
 
 	@Before
 	public void init() {
 		
 		posicionComunaValida = new Point(-34.614978, -58.372815);
 		posicionComunaInvalida = new Point(-34.620999, -58.416590);
+		cgpAdapter = new CgpAdapter(servicioExternoCGP);
+		criterioComuna = new CriterioComuna(1);
 		dispositivoConPosicionDeComunaValida = new Dispositivo("dispositivoValido", posicionComunaValida);
 		dispositivoConPosicionDeComunaValida2 = new Dispositivo("dispositivoValido2", posicionComunaValida);
 		dispositivoConPosicionDeComunaInvalida = new Dispositivo("dispostivoValido3", posicionComunaInvalida);
@@ -37,8 +45,18 @@ public class CriterioComunaTest {
 	
 		manejadorDeDispositivos = ManejadorDeDispositivos.getInstance();
 		manejadorDeDispositivos.setListaDispositivos(dispositivosValidos);
-		
-		criterioComuna = new CriterioComuna(1);
+		manejadorDeDispositivos.setCgpAdapter(cgpAdapter);
+	
+
+	}
+	
+	@Test
+	public void siFiltroDispositivosPorComunaValidaDevuelveUnaListaNoVaciaDeDispositivos(){
+		try {
+			Assert.assertEquals(2, criterioComuna.filtrar().size(),0);
+		} catch (Exception e) {
+						
+		}
 
 	}
 
