8d4ea87a09c1b4c007facb3c1bd080008e847892
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-07-06 17:40:17 -0300

    Arreglo Before de AgregarAccionesUsuarioTest

--- src/test/java/tests/AgregarAccionesUsuarioTest.java
+++ src/test/java/tests/AgregarAccionesUsuarioTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import org.uqbar.geodds.Point;
 
 import adapters.AdapterMail;
+import adapters.CgpAdapter;
+import adapters.ServicioExternoCGP;
 import eventosBusqueda.InteresadoEnBusquedas;
 import eventosBusqueda.NotificadorEmail;
 import pois.Dispositivo;
@@ -38,10 +40,30 @@ public class AgregarAccionesUsuarioTest {
 	private AccionAgregarAccionesParaLosUsuarios accionValidaTodosLosUsuarios;
 	private AccionAgregarAccionesParaLosUsuarios accionValidaUsuariosSeleccionados;
 	private List<Dispositivo> dispositivosValidos;
+	private CgpAdapter cgpAdapter;
+	private ServicioExternoCGP servicioExternoCGP;
 	
 	@Before
 	public void init(){
-		comunaValida = new CriterioComuna(1);
+		posicionComunaValida = new Point(-34.621891, -58.509017);
+		posicionComunaInvalida = new Point(-34.620999, -58.416590);
+		
+		dispositivoConPosicionDeComunaValida = new Dispositivo("dispositivoValido", posicionComunaValida);
+		dispositivoConPosicionDeComunaValida2 = new Dispositivo("dispositivoValido2", posicionComunaValida);
+		dispositivoConPosicionDeComunaInvalida = new Dispositivo("dispostivoValido3", posicionComunaInvalida);
+		
+		cgpAdapter = new CgpAdapter(servicioExternoCGP);
+		
+		dispositivosValidos = new ArrayList<Dispositivo>();
+		dispositivosValidos.add(dispositivoConPosicionDeComunaValida);
+		dispositivosValidos.add(dispositivoConPosicionDeComunaValida2);
+		dispositivosValidos.add(dispositivoConPosicionDeComunaInvalida);
+		
+		manejadorDeDispositivos = ManejadorDeDispositivos.getInstance();
+		manejadorDeDispositivos.setListaDispositivos(dispositivosValidos);
+		manejadorDeDispositivos.setCgpAdapter(cgpAdapter);
+		
+		comunaValida = new CriterioComuna(10);
 		comunaInvalida = new CriterioComuna(100);
 
 		accionValida = new NotificadorEmail(5.0, "emailAdmin@hotmail.com", adapterMail);
@@ -51,20 +73,6 @@ public class AgregarAccionesUsuarioTest {
 		dispositivosSeleccionados.add(dispositivoConPosicionDeComunaInvalida);
 		
 		usuariosSeleccionados = new CriterioUsuariosSeleccionados(dispositivosSeleccionados);
-
-		dispositivoConPosicionDeComunaValida = new Dispositivo("dispositivoValido", posicionComunaValida);
-		dispositivoConPosicionDeComunaValida2 = new Dispositivo("dispositivoValido2", posicionComunaValida);
-		dispositivoConPosicionDeComunaInvalida = new Dispositivo("dispostivoValido3", posicionComunaInvalida);
-		posicionComunaValida = new Point(-34.614978, -58.372815);
-		posicionComunaInvalida = new Point(-34.620999, -58.416590);
-		
-		dispositivosValidos = new ArrayList<Dispositivo>();
-		dispositivosValidos.add(dispositivoConPosicionDeComunaValida);
-		dispositivosValidos.add(dispositivoConPosicionDeComunaValida2);
-		dispositivosValidos.add(dispositivoConPosicionDeComunaInvalida);
-		
-		manejadorDeDispositivos = ManejadorDeDispositivos.getInstance();
-		manejadorDeDispositivos.setListaDispositivos(dispositivosValidos);
 		
 		accionValidaConComunaValida = new AccionAgregarAccionesParaLosUsuarios(accionValida, comunaValida);
 		accionValidaConComunaInvalida = new AccionAgregarAccionesParaLosUsuarios(accionValida, comunaInvalida);
@@ -77,13 +85,10 @@ public class AgregarAccionesUsuarioTest {
 		try {
 			accionValidaConComunaValida.ejecutar();
 		} catch (Exception e) {
-			e.printStackTrace();
 		}
 		
-		Assert.assertEquals(1, dispositivoConPosicionDeComunaValida.getObservers().size(),0);
-		Assert.assertEquals(1, dispositivoConPosicionDeComunaValida2.getObservers().size(),0);
-//		Assert.assertTrue(dispositivoConPosicionDeComunaValida.getObservers().contains(accionValida));
-//		Assert.assertTrue(dispositivoConPosicionDeComunaValida2.getObservers().contains(accionValida));
+		Assert.assertTrue(dispositivoConPosicionDeComunaValida.getObservers().contains(accionValida));
+		Assert.assertTrue(dispositivoConPosicionDeComunaValida2.getObservers().contains(accionValida));
 	}
 	
 	@Test
@@ -91,7 +96,6 @@ public class AgregarAccionesUsuarioTest {
 		try {
 			accionValidaConComunaInvalida.ejecutar();
 		} catch (Exception e) {
-			e.printStackTrace();
 		}
 		
 		Assert.assertTrue(dispositivoConPosicionDeComunaValida.getObservers().isEmpty());
