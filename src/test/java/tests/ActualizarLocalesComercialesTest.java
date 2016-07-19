7a2a18e77eecf500f59af5f4b219b24f7e486e03
Juan Larosa <juanlarosa96@hotmail.com>
2016-07-19 19:05:23 -0300

    Agrego test de caso en que el archivo esta vacio en ActualizarLocalesComercialesTest

--- src/test/java/tests/ActualizarLocalesComercialesTest.java
+++ src/test/java/tests/ActualizarLocalesComercialesTest.java
@@ -1,5 +1,11 @@
 package tests;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import org.junit.*;
 import fixtures.FixtureComercio;
@@ -10,42 +16,53 @@ import procesos.AccionActualizarLocalesComerciales;
 import procesos.ExceptionErrorEjecucionDeAccion;
 
 public class ActualizarLocalesComercialesTest {
-	
-	private String ruta ;
+
+	private String ruta;
+	private Comercio comercioValido;
+	private Comercio otroComercioValido;
 
 	@Before
 	public void init() {
 		ManejadorDePois.getInstance().setListaPois(new ArrayList<POI>());
+		ArrayList<String> listaEtiquetas = new ArrayList<String>();
+		listaEtiquetas.add("EtiquetaValida");
+		listaEtiquetas.add("EtiquetaValida2");
+		ArrayList<String> otraListaEtiquetas = new ArrayList<String>();
+		otraListaEtiquetas.add("OtraEtiquetaValida");
+		otraListaEtiquetas.add("OtraEtiquetaValida2");
+		comercioValido = new Comercio(null, null, null, "LocalValido", null, listaEtiquetas);
+		otroComercioValido = new Comercio(null, null, null, "OtroLocalValido", null, otraListaEtiquetas);
+
 		ruta = "./PruebaEtiquetasLocal";
+		new File(ruta).delete();
+		BufferedWriter escritor;
+		try {
+			escritor = new BufferedWriter(new FileWriter(ruta));
+			escritor.write("LocalValido;EtiquetaValida");
+			escritor.newLine();
+			escritor.close();
+		} catch (IOException e) {
+		}
+
 	}
 
 	@Test
 	public void testEjecutoActualizarLocalesComercialesYModificaPoisEnListaDePois() throws ExceptionErrorEjecucionDeAccion {
-		
-		ArrayList<String> listaEtiquetas = new ArrayList<String>();
-		listaEtiquetas.add("EtiquetaValida");
-		listaEtiquetas.add("EtiquetaValida2");
-		Comercio comercio = new Comercio(null,null,null,"LocalValido",null, listaEtiquetas);
-		ManejadorDePois.getInstance().agregarPoi(comercio);
+
+		ManejadorDePois.getInstance().agregarPoi(comercioValido);
 		AccionActualizarLocalesComerciales accion = new AccionActualizarLocalesComerciales(ruta);
 
 		Assert.assertEquals(1, accion.ejecutar().getCantElementosAfectados(), 0);
 		Assert.assertEquals(1, ManejadorDePois.getInstance().listaPois.get(0).getEtiquetas().size(), 0);
-		Assert.assertTrue(ManejadorDePois.getInstance().listaPois.get(0).getEtiquetas().get(0).equals("EtiquetaValida"));
-		ManejadorDePois.getInstance().listaPois.clear();
+		Assert.assertTrue(ManejadorDePois.getInstance().listaPois.get(0).getEtiquetas().get(0).equals("EtiquetaValida"));		
 
 	}
-	
+
 	@Test
 	public void testEjecutoActualizarLocalesComercialesYNoEncuentraLocalesConNombreEnElArchivo() throws ExceptionErrorEjecucionDeAccion {
-		//Crear el archivo de prueba
-		
-		ArrayList<String> listaEtiquetas = new ArrayList<String>();
-		listaEtiquetas.add("OtraEtiquetaValida");
-		listaEtiquetas.add("OtraEtiquetaValida2");
-		Comercio comercio = new Comercio(null,null,null,"OtroLocalValido",null, listaEtiquetas);
-		ManejadorDePois.getInstance().agregarPoi(comercio);		
-		
+		// Crear el archivo de prueba
+
+		ManejadorDePois.getInstance().agregarPoi(otroComercioValido);
 		ManejadorDePois.getInstance().listaPois.get(0).setNombre("OtroLocalValido");
 		Integer cantidadEtiquetasInicial = ManejadorDePois.getInstance().listaPois.get(0).getEtiquetas().size();
 		AccionActualizarLocalesComerciales accion = new AccionActualizarLocalesComerciales(ruta);
@@ -53,10 +70,32 @@ public class ActualizarLocalesComercialesTest {
 		Assert.assertEquals(0, accion.ejecutar().getCantElementosAfectados(), 0);
 		Assert.assertEquals(cantidadEtiquetasInicial, ManejadorDePois.getInstance().listaPois.get(0).getEtiquetas().size(), 0);
 		Assert.assertTrue(ManejadorDePois.getInstance().listaPois.get(0).getEtiquetas().get(0).equals("OtraEtiquetaValida"));
-		ManejadorDePois.getInstance().listaPois.clear(); //After
+		
 
 	}
-	
-	//Testear si el archivo esta vacio
+
+	// Testear si el archivo esta vacio
+	@Test
+	public void testEjecutoActualizarLocalesComercialesYElArchivoEstaVacio() throws ExceptionErrorEjecucionDeAccion {
+		try {
+			
+			new File(ruta).delete();			
+			AccionActualizarLocalesComerciales accion = new AccionActualizarLocalesComerciales(ruta);
+			Assert.assertEquals(0, accion.ejecutar().getCantElementosAfectados(), 0);
+			
+		} catch (Exception e) {
+
+		}
+	}
+
+	@After
+	public void after() {
+		try {
+			ManejadorDePois.getInstance().listaPois.clear();
+			new File(ruta).delete();
+		} catch (Exception e) {
+
+		}
+	}
 
 }
