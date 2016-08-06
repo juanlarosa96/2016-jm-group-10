77492951f686fbceb2d1e7224852132051fe5184
Marco Di Croce <marcodicro@hotmail.com>
2016-08-06 20:35:37 -0300

    modifico ManejadorDeProcesosTest para que use FalsoScheduler

--- src/test/java/tests/ManejadorDeProcesosTest.java
+++ src/test/java/tests/ManejadorDeProcesosTest.java
@@ -2,8 +2,6 @@ package tests;
 
 import static org.mockito.Mockito.*;
 
-import java.util.concurrent.TimeUnit;
-
 import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,9 +10,7 @@ import org.junit.Test;
 import procesos.Accion;
 import procesos.ExceptionErrorEjecucionDeAccion;
 import procesos.ManejadorDeProcesos;
-import procesos.Proceso;
 import procesos.ResultadoEjecucion;
-import procesos.Scheduler;
 import procesos.SchedulerAdapter;;
 
 public class ManejadorDeProcesosTest {
@@ -22,8 +18,7 @@ public class ManejadorDeProcesosTest {
 	private Accion otraAccionValida;
 	private ManejadorDeProcesos manejadorProcesos;
 	private ResultadoEjecucion resultadoEjecucion;
-	private Proceso proceso;
-	private Scheduler scheduler;
+	private FalsoScheduler scheduler;
 	private SchedulerAdapter schedulerAdapter;
 
 	@Before
@@ -34,22 +29,20 @@ public class ManejadorDeProcesosTest {
 		accionValida = mock(Accion.class);
 		otraAccionValida = mock(Accion.class);
 
-		scheduler = mock(Scheduler.class);
+		scheduler = new FalsoScheduler();
+		scheduler.resetLlamados();
+		
 		schedulerAdapter = SchedulerAdapter.getInstance();
 		schedulerAdapter.setScheduler(scheduler);
 		
 		manejadorProcesos = ManejadorDeProcesos.getInstance();
 		manejadorProcesos.vaciarListaProcesosEjecutados();
 
-		proceso = new Proceso(accionValida, 0.0, DateTime.now(), null);
-
 		try {
 			when(accionValida.ejecutar()).thenReturn(resultadoEjecucion);
 			when(otraAccionValida.ejecutar()).thenReturn(resultadoEjecucion);
 		} catch (ExceptionErrorEjecucionDeAccion e) {
 		}
-
-		when(scheduler.schedule(any(Proceso.class), anyLong(), any(TimeUnit.class), anyDouble())).thenReturn(proceso.run());
 
 	}
 
@@ -58,8 +51,9 @@ public class ManejadorDeProcesosTest {
 
 		DateTime tiempoDeEjecucion = DateTime.now();
 		manejadorProcesos.configurarProceso(accionValida, 0.0, tiempoDeEjecucion, null);
+		
+		Assert.assertEquals(1,scheduler.cantVecesLlamado(),0);
 
-		verify(scheduler).schedule(any(Proceso.class), anyLong(), any(TimeUnit.class), anyDouble());
 		Assert.assertEquals(1, manejadorProcesos.cantProcesosEjecutados(), 0);
 
 	}
@@ -73,8 +67,8 @@ public class ManejadorDeProcesosTest {
 		manejadorProcesos.configurarProceso(accionValida, 0.0, tiempoEjecucionProceso1, null);
 		manejadorProcesos.configurarProceso(otraAccionValida, 0.0, tiempoEjecucionProceso2, null);
 
-		verify(scheduler, times(2)).schedule(any(Proceso.class), anyLong(), any(TimeUnit.class), anyDouble());
-
+		Assert.assertEquals(2,scheduler.cantVecesLlamado(),0);
+		
 		Assert.assertEquals(2, manejadorProcesos.cantProcesosEjecutados(), 0);
 
 	}
