b0f35a14d3a8eeac091c46099fbd758a7bd6a517
Marco Di Croce <marcodicro@hotmail.com>
2016-09-07 21:15:37 -0300

    Modifico Comuna para poder mapearla

--- src/main/java/pois/Comuna.java
+++ src/main/java/pois/Comuna.java
@@ -2,12 +2,32 @@ package pois;
 
 import java.util.List;
 
+import javax.persistence.*;
+
 import org.uqbar.geodds.Point;
 import org.uqbar.geodds.Polygon;
 
+@Embeddable
 public class Comuna {
+	public Polygon getTerritorio() {
+		return territorio;
+	}
+
+	public void setTerritorio(Polygon territorio) {
+		this.territorio = territorio;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
 	private Integer numero;
+	
+	//Averiguar como hacer para convertir polygon
 	private Polygon territorio;
+	
+	@SuppressWarnings("unused")
+	private Comuna(){}
 
 	public Comuna(Integer unNumero, List<Point> puntosFrontera) {
 		numero = unNumero;
