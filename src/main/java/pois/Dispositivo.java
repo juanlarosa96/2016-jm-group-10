7822c7fe13b3c7188851710d1faa270c1b578a60
Marco Di Croce <marcodicro@hotmail.com>
2016-09-07 21:16:27 -0300

    Modifico Dispositivo para poder mapearla

--- src/main/java/pois/Dispositivo.java
+++ src/main/java/pois/Dispositivo.java
@@ -3,6 +3,8 @@ package pois;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.*;
+
 import org.joda.time.DateTime;
 import org.uqbar.geodds.Point;
 
@@ -10,11 +12,27 @@ import eventosBusqueda.ResultadoBusqueda;
 import eventosBusqueda.InteresadoEnBusquedas;
 import herramientas.ManejadorDeFechas;
 
+@Table(name = "dispositivos")
+@Entity
 public class Dispositivo {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
 	private String nombre;
+
+	// Averiguar como convertir posicion
 	private Point posicion;
+
+	@Transient
 	private ManejadorDePois manejadorDePois;
+
+	@Transient
 	private List<InteresadoEnBusquedas> observers;
+	
+	@SuppressWarnings("unused")
+	private Dispositivo(){}
 
 	public Dispositivo(String unNombre, Point unaPosicion) {
 		this.nombre = unNombre;
@@ -33,31 +51,31 @@ public class Dispositivo {
 
 	public List<POI> buscarPOIs(String descripcion) {
 		DateTime inicio = DateTime.now();
-		
+
 		List<POI> listaPoisEncontrados = manejadorDePois.buscarPOIs(descripcion);
-		
+
 		DateTime fin = DateTime.now();
-		
-		Double demoraEnSegundos = ManejadorDeFechas.obtenerDuracionIntervaloEnSegundos(inicio,fin);
-		
-		ResultadoBusqueda unaBusqueda = new ResultadoBusqueda(this.nombre, listaPoisEncontrados, DateTime.now(), demoraEnSegundos,
-				descripcion);
-		
+
+		Double demoraEnSegundos = ManejadorDeFechas.obtenerDuracionIntervaloEnSegundos(inicio, fin);
+
+		ResultadoBusqueda unaBusqueda = new ResultadoBusqueda(this.nombre, listaPoisEncontrados, DateTime.now(),
+				demoraEnSegundos, descripcion);
+
 		this.notificarBusqueda(unaBusqueda);
-		
+
 		return listaPoisEncontrados;
 	}
-	
+
 	private void notificarBusqueda(ResultadoBusqueda unaBusqueda) {
 		observers.stream().forEach(observer -> observer.notificarBusqueda(unaBusqueda));
-		
+
 	}
 
-	public void agregarInteresadoEnBusquedas(InteresadoEnBusquedas unInteresado){
+	public void agregarInteresadoEnBusquedas(InteresadoEnBusquedas unInteresado) {
 		observers.add(unInteresado);
 	}
-	
-	public void eliminarInteresadoEnBusquedas(InteresadoEnBusquedas unInteresado){
+
+	public void eliminarInteresadoEnBusquedas(InteresadoEnBusquedas unInteresado) {
 		observers.remove(unInteresado);
 	}
 
@@ -65,25 +83,16 @@ public class Dispositivo {
 		return posicion;
 	}
 	
-	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setPosicion(Point posicion) {
+		this.posicion = posicion;
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
