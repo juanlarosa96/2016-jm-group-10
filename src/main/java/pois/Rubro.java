4e54c6bfb707a035cbfad01b4ac0a08487d03188
Marco Di Croce <marcodicro@hotmail.com>
2016-09-07 21:17:54 -0300

    Modifico Rubro para poder mapearla

--- src/main/java/pois/Rubro.java
+++ src/main/java/pois/Rubro.java
@@ -1,8 +1,19 @@
 package pois;
 
+import javax.persistence.*;
+
+@Table(name = "rubros")
+@Entity
 public class Rubro {
+	
+	@Id @GeneratedValue
+	private Integer id;
+	
 	private String nombreRubro;
 	private Double condicionDeCercania;
+	
+	@SuppressWarnings("unused")
+	private Rubro(){}
 
 	public Rubro(String nombre, Double radioDeCercania) {
 		this.nombreRubro = nombre;
@@ -17,4 +28,12 @@ public class Rubro {
 		return nombreRubro;
 	}
 
+	public void setNombreRubro(String nombreRubro) {
+		this.nombreRubro = nombreRubro;
+	}
+
+	public void setCondicionDeCercania(Double condicionDeCercania) {
+		this.condicionDeCercania = condicionDeCercania;
+	}
+	
 }
