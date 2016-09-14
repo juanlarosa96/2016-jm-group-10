cc9a341053783b819686be6595806d39469a3264
Marco Di Croce <marcodicro@hotmail.com>
2016-09-14 16:57:27 -0300

    update

--- src/test/java/tests/ContextTest.java
+++ src/test/java/tests/ContextTest.java
@@ -1,16 +1,21 @@
 package tests;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class ContextTest {
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
+public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
 
 	@Test
 	public void contextUp() {
-		EntityManagerHelper.getEntityManager();
+		assertNotNull(entityManager());
 	}
 
 	@Test
 	public void contextUpWithTransaction() throws Exception {
-		EntityManagerHelper.withTransaction(() -> {});
+		withTransaction(() -> {});
 	}
+
 }
