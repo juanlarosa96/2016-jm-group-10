388aade67c407a5732cffe32bd6abaf628be31f3
Marco Di Croce <marcodicro@hotmail.com>
2016-09-17 18:45:14 -0300

    agrego transacciones al ManejadorDePois. Ademas al buscar trabaja con su lista de pois. Si esta sufre algun cambio, updatea a la bd

--- src/main/java/pois/ManejadorDePois.java
+++ src/main/java/pois/ManejadorDePois.java
@@ -2,18 +2,12 @@ package pois;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.persistence.EntityManager;
 
 import org.joda.time.DateTime;
-import org.omg.CORBA.PERSIST_STORE;
 
 import adapters.ComponenteExternoAdapter;
-import tests.EntityManagerHelper;
-
+import herramientas.EntityManagerHelper;
 
 public class ManejadorDePois {
 
@@ -22,7 +16,12 @@ public class ManejadorDePois {
 	private List<ComponenteExternoAdapter> adaptersComponentesExternos;
 
 	private ManejadorDePois() {
-		listaPois= new ArrayList<POI>();
+		this.inicializarListaPois();
+	}
+
+	@SuppressWarnings("unchecked")
+	private void inicializarListaPois() {
+		listaPois = EntityManagerHelper.createQuery("from POI").getResultList();
 	}
 
 	public static ManejadorDePois getInstance() {
@@ -33,53 +32,60 @@ public class ManejadorDePois {
 		return singleton;
 	}
 
-   public void setListaPois(List<POI> unaListaDePois) {
-	
-		listaPois = unaListaDePois;
-		unaListaDePois.stream().forEach(poi -> EntityManagerHelper.getEntityManager().persist(poi));
-	}
+	@SuppressWarnings("unchecked")
+	public void setListaPois(List<POI> listaPoisNueva) {
 
+		listaPois = listaPoisNueva;
+
+		EntityManagerHelper.beginTransaction();
+
+		List<POI> poisActuales = EntityManagerHelper.createQuery("from POI").getResultList();
+		poisActuales.stream().forEach(poi -> EntityManagerHelper.remove(poi));
+
+		listaPoisNueva.stream().forEach(poi -> EntityManagerHelper.persist(poi));
+
+		EntityManagerHelper.commit();
+
+	}
 
 	public void setListaAdapters(List<ComponenteExternoAdapter> listaAdapters) {
 		adaptersComponentesExternos = listaAdapters;
 	}
 
 	public void agregarPoi(POI poi) {
-		if(this.estaEnLaLista(poi))
+		if (this.estaEnLaLista(poi))
 			actualizarPoi(poi);
-		else{
+		else {
 			listaPois.add(poi);
-			EntityManagerHelper.getEntityManager().persist(poi);
+			this.persistirPOI(poi);
+		}
+
 	}
-		
+
+	private void persistirPOI(POI poi) {
+		EntityManagerHelper.beginTransaction();
+		EntityManagerHelper.persist(poi);
+		EntityManagerHelper.commit();
 	}
 
 	private void actualizarPoi(POI poiNuevo) {
-		
+
 		POI poiViejoLista = listaPois.stream().filter(unPoi -> unPoi.esIgualA(poiNuevo)).findFirst().get();
-		
-		this.eliminarPOI(poiViejoLista);
-		
+
+		listaPois.remove(poiViejoLista);
 		listaPois.add(poiNuevo);
-		
-		EntityManager em = EntityManagerHelper.getEntityManager(); 
- 		POI poiViejo = em.find(POI.class, poiNuevo.getId());
- 		poiViejo.copiarEstado(poiNuevo);
-		em.flush();
-		
-				
+
+		EntityManagerHelper.beginTransaction();
+		POI poiViejo = EntityManagerHelper.find(POI.class, poiNuevo.getId());
+		poiViejo.copiarEstado(poiNuevo);
+		EntityManagerHelper.flush();
+		EntityManagerHelper.commit();
+
 	}
 
 	private boolean estaEnLaLista(POI poiBuscado) {
-				return listaPois.stream().anyMatch(unPoi -> poiBuscado.esIgualA(unPoi));
-		/*POI poi =  EntityManagerHelper.getEntityManager().find(POI.class, poiBuscado.getId());
-	if(poi != null){
-		return true;
+		return listaPois.stream().anyMatch(unPoi -> poiBuscado.esIgualA(unPoi));
 	}
-	return false;
-
-	}
-	*/}
 
 	private void consultarPoisExternos(String descripcion) {
 
@@ -104,15 +110,19 @@ public class ManejadorDePois {
 
 	public void eliminarPOI(POI poi) {
 		listaPois.remove(poi);
-		EntityManagerHelper.getEntityManager().remove(poi);
+		this.eliminarPoiDeBD(poi);
+	}
+
+	private void eliminarPoiDeBD(POI poi) {
+		EntityManagerHelper.beginTransaction();
+		EntityManagerHelper.remove(poi);
+		EntityManagerHelper.commit();
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<POI> buscarPOIs(String descripcion) {
 		this.consultarPoisExternos(descripcion);
-		List<POI> listaPois = EntityManagerHelper.getEntityManager().createQuery("from POI").getResultList();
 		return listaPois.stream().filter(poi -> poi.contiene(descripcion)).collect(Collectors.toList());
-		
 
 	}
 
@@ -132,23 +142,27 @@ public class ManejadorDePois {
 
 	@SuppressWarnings("unchecked")
 	public Integer actualizarEtiquetasLocalesComerciales(String nombre, List<String> etiquetas) {
-		EntityManager em = EntityManagerHelper.getEntityManager(); 
-		List<POI> lista = em.createQuery("from comercios where nombre = :nombre").setParameter("nombre", nombre).getResultList();			
-		lista.stream().forEach(comercio -> comercio.setEtiquetas(etiquetas));
-		em.flush();
-		return lista.size();
+
+		EntityManagerHelper.beginTransaction();
+
+		List<Comercio> comercios = EntityManagerHelper.createQuery("from Comercio where nombre = :nombre")
+				.setParameter("nombre", nombre).getResultList();
+
+		comercios.stream().forEach(comercio -> comercio.setEtiquetas(etiquetas));
+
+		EntityManagerHelper.flush();
+		EntityManagerHelper.commit();
+
+		return comercios.size();
 
 	}
 
 	@SuppressWarnings("unchecked")
 	public POI buscarPOI(String nombrePOI, Direccion direccionPOI) {
-		return listaPois.stream().filter(
-				poi -> poi.getNombre().equalsIgnoreCase(nombrePOI) && poi.getDireccion().esLaMismaDireccionQue(direccionPOI))
-				.collect(Collectors.toList()).get(0);
-		/*List<POI> listaPois = EntityManagerHelper.getEntityManager().createQuery("from pois where nombre = :nombrePOI").setParameter("nombrePOI", nombrePOI).getResultList();
-		return listaPois.stream().filter(poi -> poi.getDireccion().esLaMismaDireccionQue(direccionPOI)).collect(Collectors.toList()).get(0);
-		*/
 		// Si no encuentra ninguno tira IndexOutOfBoundsException
+
+		return listaPois.stream().filter(poi -> poi.getNombre().equalsIgnoreCase(nombrePOI)
+				&& poi.getDireccion().esLaMismaDireccionQue(direccionPOI)).collect(Collectors.toList()).get(0);
 	}
 
 }
