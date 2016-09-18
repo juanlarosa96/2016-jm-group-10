418ad503e37fe170f26c698e130f718d6a3f0f40
Marco Di Croce <marcodicro@hotmail.com>
2016-09-17 22:57:49 -0300

    Agrego manejo de persistencia al ManejadorDeDispositivos

--- src/main/java/pois/ManejadorDeDispositivos.java
+++ src/main/java/pois/ManejadorDeDispositivos.java
@@ -4,14 +4,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import adapters.CgpAdapter;
+import herramientas.EntityManagerHelper;
 
 public class ManejadorDeDispositivos {
 	private static ManejadorDeDispositivos singleton = null;
 	public List<Dispositivo> listaDispositivos;
 	private CgpAdapter cgpAdapter;
 
+	@SuppressWarnings("unchecked")
 	private ManejadorDeDispositivos() {
+		EntityManagerHelper.beginTransaction();
 
+		listaDispositivos = EntityManagerHelper.createQuery("from Dispositivo").getResultList();
+
+		EntityManagerHelper.commit();
 	}
 
 	public static ManejadorDeDispositivos getInstance() {
@@ -30,12 +36,26 @@ public class ManejadorDeDispositivos {
 		return listaDispositivos;
 	}
 
-	public void setListaDispositivos(List<Dispositivo> unaListaDeDispositivos) {
-		listaDispositivos = unaListaDeDispositivos;
+	public void setListaDispositivos(List<Dispositivo> nuevaListaDispositivos) {
+		listaDispositivos = nuevaListaDispositivos;
+
+		EntityManagerHelper.beginTransaction();
+
+		@SuppressWarnings("unchecked")
+		List<Dispositivo> dispViejos = EntityManagerHelper.createQuery("from Dispositivo").getResultList();
+		dispViejos.stream().forEach(disp -> EntityManagerHelper.remove(disp));
+
+		nuevaListaDispositivos.stream().forEach(poi -> EntityManagerHelper.persist(poi));
+
+		EntityManagerHelper.commit();
 	}
 
 	public void agregarDispositivo(Dispositivo dispositivo) {
 		listaDispositivos.add(dispositivo);
+
+		EntityManagerHelper.beginTransaction();
+		EntityManagerHelper.persist(dispositivo);
+		EntityManagerHelper.commit();
 	}
 
 	public List<Dispositivo> filtrarPorComuna(Integer numComuna) throws Exception {
@@ -49,5 +69,15 @@ public class ManejadorDeDispositivos {
 		List<Dispositivo> dispositivos = this.listaDispositivos.stream()
 				.filter(dispositivo -> comuna.incluyeA(dispositivo.getPosicion())).collect(Collectors.toList());
 		return dispositivos;
+	}
+
+	public void eliminarDispositivo(Dispositivo disp) {
+		listaDispositivos.remove(disp);
+		
+		EntityManagerHelper.beginTransaction();
+		EntityManagerHelper.find(Dispositivo.class, disp.getId());
+		EntityManagerHelper.remove(disp);
+		EntityManagerHelper.commit();
+				
 	}
 }
