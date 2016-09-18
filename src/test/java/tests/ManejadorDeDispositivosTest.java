2d5f90408bf25a412804783d5e655665f3a054a3
Marco Di Croce <marcodicro@hotmail.com>
2016-09-17 22:59:49 -0300

    refactor tests ManejadorDeDispositivos

--- src/test/java/tests/ManejadorDeDispositivosTest.java
+++ src/test/java/tests/ManejadorDeDispositivosTest.java
@@ -1,47 +1,51 @@
 package tests;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
 import pois.Dispositivo;
+import pois.ManejadorDeDispositivos;
 import pois.Posicion;
 
 public class ManejadorDeDispositivosTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
 
+	private ManejadorDeDispositivos manejadorDeDispositivos;
+
+	@Before
+	public void init() {
+		manejadorDeDispositivos = ManejadorDeDispositivos.getInstance();
+	}
+
 	@Test
-	public void SiPersistoUnDispositivoLuegoLoEncuentro() {
+	public void SiAgregoUnDispositivoLoEncuentroEnBD() {
 
 		Dispositivo disp = new Dispositivo("unDisp", new Posicion(50.5, 52.5));
 
-		entityManager().persist(disp);
+		manejadorDeDispositivos.agregarDispositivo(disp);
 
 		Dispositivo dispEncontrado = entityManager().find(Dispositivo.class, disp.getId());
 
 		Assert.assertTrue(disp.getNombre().equals(dispEncontrado.getNombre()));
 
-		System.out.println("nombre: " + dispEncontrado.getNombre() + "; pos: " + dispEncontrado.getPosicion().toString()
-				+ "; id: " + dispEncontrado.getId().toString());
-
+		Assert.assertTrue(disp.getId() == dispEncontrado.getId());
 	}
 
 	@Test
-	public void SiPersistoDosDispositivosLosEncuentro() {
+	public void SiAgregoUnDispositivoYLuegoLoEliminoEntoncesNoLoEncuentroEnBD() {
+
+		Dispositivo disp = new Dispositivo("unDisp", new Posicion(25.5, 25.5));
+
+		manejadorDeDispositivos.agregarDispositivo(disp);
+		manejadorDeDispositivos.eliminarDispositivo(disp);
+
+		Dispositivo dispEncontrado = entityManager().find(Dispositivo.class, disp.getId());
+
+		Assert.assertTrue(dispEncontrado == null);
+		Assert.assertFalse(manejadorDeDispositivos.getListaDispositivos().contains(disp));
 		
-		Dispositivo disp1 = new Dispositivo("unDisp", new Posicion(50.5, 52.5));
-		
-		Dispositivo disp2 = new Dispositivo("unDisp", new Posicion(50.5, 52.5));
-
-		entityManager().persist(disp1);
-		entityManager().persist(disp2);
-
-		Dispositivo dispEncontrado1 = entityManager().find(Dispositivo.class, disp1.getId());
-		Dispositivo dispEncontrado2 = entityManager().find(Dispositivo.class, disp2.getId());
-
-		Assert.assertTrue(disp1.getNombre().equals(dispEncontrado1.getNombre()));
-		Assert.assertTrue(disp2.getNombre().equals(dispEncontrado2.getNombre()));
-
 	}
 
 }
