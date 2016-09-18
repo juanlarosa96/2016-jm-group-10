0dce1ca34b302dc508d0115f9d7156d4aad5eaa9
Marco Di Croce <marcodicro@hotmail.com>
2016-09-18 12:51:21 -0300

    MailEnviado es embeddable. Agrego constructor vacio y getters/setters

--- src/main/java/eventosBusqueda/MailEnviado.java
+++ src/main/java/eventosBusqueda/MailEnviado.java
@@ -1,7 +1,9 @@
 package eventosBusqueda;
 
+import javax.persistence.Embeddable;
 import org.joda.time.LocalDateTime;
 
+@Embeddable
 public class MailEnviado {
 
 	private String mailOrigen;
@@ -10,7 +12,49 @@ public class MailEnviado {
 	private LocalDateTime fecha;
 	private String mensaje;
 	
+	@SuppressWarnings("unused")
+	private MailEnviado(){}
 	
+	public String getMailOrigen() {
+		return mailOrigen;
+	}
+
+	public void setMailOrigen(String mailOrigen) {
+		this.mailOrigen = mailOrigen;
+	}
+
+	public String getMailDestinatario() {
+		return mailDestinatario;
+	}
+
+	public void setMailDestinatario(String mailDestinatario) {
+		this.mailDestinatario = mailDestinatario;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
 	public MailEnviado(String mailOrigen, String mailDestinatario, String asunto, LocalDateTime fecha, String mensaje) {
 		this.mailOrigen = mailOrigen;
 		this.mailDestinatario = mailDestinatario;
