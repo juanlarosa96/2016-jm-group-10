94243a56a0b73beb5e868559baa28e1d8ab1044b
Sofía Cortés <sofiavcortes@gmail.com>
2016-09-20 18:05:03 -0300

    Agrego test de persistencia para NotificadorEmail

---
+++ src/test/java/tests/NotificadorEmailTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import static org.mockito.Mockito.mock;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import adapters.AdapterMail;
+import eventosBusqueda.NotificadorEmail;
+import eventosBusqueda.ResultadoBusqueda;
+import herramientas.EntityManagerHelper;
+import pois.POI;
+
+public class NotificadorEmailTest {
+	
+		private AdapterMail adapterMailMockeado;
+		private NotificadorEmail notificadorEmail;
+		private ResultadoBusqueda busqueda1;
+
+		@Before
+		public void init() {
+			adapterMailMockeado = mock(AdapterMail.class);	
+			
+			busqueda1 = new ResultadoBusqueda("terminalAbasto", new ArrayList<POI>() , null, 10.0, "hospital");
+			
+			notificadorEmail = new NotificadorEmail(5.0, "admin@gmail.com", adapterMailMockeado);
+			
+			EntityManagerHelper.persist(notificadorEmail);
+
+		}
+		
+		@Test
+		public void siNotificoUnaBusquedaEncuentroElMailEnLaLista(){
+			notificadorEmail.notificarBusqueda(busqueda1);
+			Assert.assertEquals(notificadorEmail.getMailsEnviados().size(),1,0);
+			EntityManagerHelper.beginTransaction();
+			EntityManagerHelper.flush();
+			EntityManagerHelper.getEntityManager().detach(notificadorEmail);
+			NotificadorEmail notificadorEmailEncontrado = EntityManagerHelper.getEntityManager().find(NotificadorEmail.class, notificadorEmail.getId());
+			EntityManagerHelper.commit();
+			Assert.assertEquals(notificadorEmailEncontrado.getMailsEnviados().size(),1,0);
+		}
+		
+	}
+
+	
