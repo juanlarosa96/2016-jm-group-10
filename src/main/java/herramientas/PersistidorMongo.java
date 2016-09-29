9e19e894a81a9fa47edf7d31af6483a4dddf2c3e
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-09-29 19:04:21 -0300

    Agrego la clase PersistidorMongo

---
+++ src/main/java/herramientas/PersistidorMongo.java
@@ -0,0 +1,38 @@
+package herramientas;
+
+import java.util.List;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.query.Query;
+
+import com.mongodb.MongoClient;
+
+import eventosBusqueda.ResultadoBusqueda;
+
+public class PersistidorMongo {
+	
+	private Morphia persistidor = new Morphia();
+	private Datastore datastore;
+
+	public void inicializarDB(String nombreDB) {
+		persistidor.mapPackage("eventosBusqueda");
+		persistidor.mapPackage("pois");
+		datastore = persistidor.createDatastore(new MongoClient(), nombreDB);
+		datastore.ensureIndexes();
+	}
+
+	public List<ResultadoBusqueda> buscarTodosLosResultadosBusqueda() {
+		return datastore.createQuery(ResultadoBusqueda.class).asList();
+	}
+
+	public void guardar(ResultadoBusqueda busq) {
+		datastore.save(busq);
+	}
+
+	public void borrarTodosLosResultadosBusquedaDeBD() {
+		Query<ResultadoBusqueda> resultadosABorrar = datastore.createQuery(ResultadoBusqueda.class);
+		datastore.delete(resultadosABorrar);
+	}
+
+}
