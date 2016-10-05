4753806b24cbf6b81d26584d227a2f4e8958919f
JulianIP <jipandullo@gmail.com>
2016-10-05 15:26:12 -0300

    Quito metodos de conversion de json a poi y poi a json trasladandolos a jsonPoiConverter

--- src/main/java/herramientas/JedisHelper.java
+++ src/main/java/herramientas/JedisHelper.java
@@ -2,13 +2,20 @@ package herramientas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import converters.JsonPoiConverter;
 import pois.Banco;
 import pois.CGP;
+import pois.FranjaHoraria;
 import pois.POI;
+import pois.Servicio;
 import redis.clients.jedis.Jedis;
 
 public class JedisHelper {
@@ -18,64 +25,25 @@ public class JedisHelper {
 	public static void persistirPoiExterno(POI unPoi) {
 
 		conectarARedis();
-		String gsonAPersistir = convertirDePOIAJson(unPoi);
-		jedis.lpush("IdPoiExterno", gsonAPersistir);
-
-	}
-
-	public static String convertirDePOIAJson(POI unPoi) {
-
-		Gson gson = new Gson();
-		JsonElement jsonElement = gson.toJsonTree(unPoi);
-		jsonElement.getAsJsonObject().remove("horarios");
-		jsonElement.getAsJsonObject().addProperty("tipoPoi", unPoi.getClass().getName());
-		String gsonConvertido = gson.toJson(jsonElement);
-		return gsonConvertido;
+		JsonObject gsonAPersistir = JsonPoiConverter.convertirDePOIAJson(unPoi);
+		jedis.rpush("IdPoiExterno", gsonAPersistir.toString());
 
 	}
 
 	public static List<POI> obtenerPoisExternosDeRedis() {
 		conectarARedis();
-		List<POI> listaBancos = new ArrayList<POI>();
-		List<String> bancosObtenidos = new ArrayList<String>();
+		List<POI> listaPOI = new ArrayList<POI>();
+		List<String> poisObtenidos = new ArrayList<String>();
 
-		bancosObtenidos.addAll(jedis.lrange("IdPoiExterno", 0, jedis.llen("IdPoiExterno")));
+		poisObtenidos.addAll(jedis.lrange("IdPoiExterno", 0, jedis.llen("IdPoiExterno")));
 
-		for (int i = 0; i < bancosObtenidos.size(); i++) {
+		for (int i = 0; i < poisObtenidos.size(); i++) {
 
-			listaBancos.add(convertirDeJsonAPOI(bancosObtenidos.get(i)));
+			listaPOI.add(JsonPoiConverter.convertirDeJsonAPOI(poisObtenidos.get(i)));
 
 		}
 
-		return listaBancos;
-	}
-
-	public static POI convertirDeJsonAPOI(String unPoi) {
-
-		Gson gson = new Gson();
-		JsonElement jsonElement = gson.fromJson(unPoi, JsonElement.class);
-		Class clasePoi;
-		
-
-		String nombreClase = jsonElement.getAsJsonObject().remove("tipoPoi").toString();
-		nombreClase = nombreClase.substring(1, nombreClase.length()-1);		
-		try {
-			clasePoi = Class.forName(nombreClase);
-
-			if (clasePoi == Banco.class) {
-				Banco unBanco = gson.fromJson(unPoi, Banco.class);
-				unBanco.setHorarios(unBanco.horariosBancos());
-				return unBanco;
-			} else {
-				CGP unCgp = gson.fromJson(unPoi, CGP.class);
-				// Magia con los horarios
-				return unCgp;
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-
+		return listaPOI;
 	}
 
 	public static void conectarARedis() {
@@ -95,17 +63,17 @@ public class JedisHelper {
 
 	public static void actualizarPoiExterno(POI poiExternoNuevo, POI poiViejoLista) {
 		conectarARedis();
-		// HACER EL POP Y PUSH
+		// hacer
 
 	}
 
 	public static void limpiarBaseDeDatosRedis() {
-		jedis.del("IdPoiExternos");
-
+		jedis.flushAll();
 	}
 
 	public static Long obtenerCantidadPersistida() {
 		return jedis.llen("IdPoiExterno");
 	}
+	
 
 }
