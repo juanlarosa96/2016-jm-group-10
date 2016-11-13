cd92e3334d8e936cbffa2f4b1374462b3879f194
Marco Di Croce <marcodicro@hotmail.com>
2016-11-13 20:57:16 -0300

    agrego rutas al router

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -1,7 +1,9 @@
 package server;
 
+import controllers.ConsultasController;
 import controllers.LoginController;
 import controllers.PoisController;
+import controllers.TerminalesController;
 import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.utils.HandlebarsTemplateEngineBuilder;
@@ -19,15 +21,21 @@ public class Router {
 		//controllers
 		
 		PoisController poisController = new PoisController();
+		TerminalesController terminalesController = new TerminalesController();
+		ConsultasController consultasController = new ConsultasController();
 		
 		//rutas		
 		
 		Spark.get("/", LoginController::login, engine);
 		Spark.get("/login/", LoginController::loginUsuario, engine);
-		Spark.get("/buscarPois", poisController::buscarPois, engine);
-		Spark.get("/administrarPois", poisController::administrarPois, engine);
+		Spark.get("/buscarPois/", poisController::buscarPois, engine);
 		
 		Spark.get("/pois/:id", poisController::mostrarPOI, engine);
+		
+		Spark.get("/admin/administrarPois", poisController::administrarPois, engine);
+		Spark.get("/admin/administrarTerminales", terminalesController::administrarTerminales, engine);
+		Spark.get("/admin/historicoConsultas", consultasController::historicoConsultas, engine);
+		
 		
 		
 	}
