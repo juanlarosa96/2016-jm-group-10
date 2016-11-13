09210bf784e73fe6abbeb9b4bee117ce0efca6a7
Marco Di Croce <marcodicro@hotmail.com>
2016-11-12 23:06:44 -0300

    agrego clases rooter,handlebarsTemplateEngineBuilder

---
+++ src/main/java/server/Router.java
@@ -0,0 +1,27 @@
+package server;
+
+import controllers.LoginController;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import spark.utils.HandlebarsTemplateEngineBuilder;
+
+public class Router {
+
+	public static void configure() {
+		HandlebarsTemplateEngine engine = HandlebarsTemplateEngineBuilder
+				.create()
+				.withDefaultHelpers()
+				.build();
+
+		Spark.staticFiles.location("/public");
+		
+		//controllers
+		
+		//rutas		
+		
+		Spark.get("/", LoginController::login, engine);
+		
+		
+	}
+
+}
