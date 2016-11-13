3010aba44f451f776d9675eb297c417ec71020d8
Marco Di Croce <marcodicro@hotmail.com>
2016-11-12 23:16:55 -0300

    agrego Bootstrap,Server, layout.hbs,login.hbs,main.css

---
+++ src/main/java/server/Bootstrap.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
+
+public class Bootstrap implements WithGlobalEntityManager, EntityManagerOps, TransactionalOps{
+	
+	public void init(){
+		/*withTransaction(() ->{
+			//agregar objetos a bd		
+		});*/
+	}
+}
