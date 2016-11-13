30a5442df9f2583eed382d057afe3b169c9a518a
Marco Di Croce <marcodicro@hotmail.com>
2016-11-13 01:34:00 -0300

    agrego login controller

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,12 +1,41 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import usuarios.ExceptionErrorLogin;
+import usuarios.RepoUsuarios;
+import usuarios.Usuario;
 
 public class LoginController {
 
 	public static ModelAndView login(Request req, Response res){
 		return new ModelAndView(null, "login/login.hbs");
+	}
+	
+	public static ModelAndView loginUsuario(Request req, Response res) throws ExceptionErrorLogin{
+		RepoUsuarios repoUsuarios = RepoUsuarios.getInstance();
+		String username = req.params("username");		
+		String password = req.params("password");
+		
+		try {
+			Usuario usuario = repoUsuarios.loginOK(username, password);
+			
+			Map<String, Usuario> model = new HashMap<>();
+			
+			model.put("usuario",usuario);
+			
+			if(usuario.esAdmin())	
+				return new ModelAndView(model, "admin/seleccionarPantallaAdmin.hbs");		
+			
+			else
+				return new ModelAndView(model, "terminal/buscarPois.hbs");			
+			
+		} catch (Exception e) {
+			throw new ExceptionErrorLogin();
+		}
 	}
 }
