64a7a4d06983cae688339828a57717048cb61e93
Marco Di Croce <marcodicro@hotmail.com>
2016-11-13 01:34:12 -0300

    agrego pois controller

--- src/main/java/controllers/PoisController.java
+++ src/main/java/controllers/PoisController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,7 +8,6 @@ import java.util.Map;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
-import pois.ManejadorDePois;
 import pois.POI;
 import spark.ModelAndView;
 import spark.Request;
@@ -18,12 +18,24 @@ public class PoisController implements WithGlobalEntityManager, TransactionalOps
 	public ModelAndView buscarPois(Request req, Response res) {
 		
 		Map<String, List<POI>> model = new HashMap<>();
+				
+		return new ModelAndView(model, "terminal/buscarPois.hbs");
+	}
+	
+public ModelAndView administrarPois(Request req, Response res) {
 		
-		List<POI> poisEncontrados = ManejadorDePois.getInstance().buscarPOIs(req.params("descripcion"));
-
-		model.put("pois", poisEncontrados);
+		Map<String, List<String>> model = new HashMap<>();
 		
-		return new ModelAndView(model, "pois/buscarPois.hbs");
+		List<String> tipos = new ArrayList<String>();
+		
+		tipos.add("Banco");
+		tipos.add("CGP");
+		tipos.add("Comercio");		
+		tipos.add("Parada Colectivo");
+		
+		model.put("tipos",tipos);
+				
+		return new ModelAndView(model, "admin/administrarPois.hbs");
 	}
 
 }
