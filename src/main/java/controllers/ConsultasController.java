40cbd41c3f723bfafacf31a06b73af85c547ac63
Marco Di Croce <marcodicro@hotmail.com>
2016-11-13 20:56:59 -0300

    agrego controller para consultas y terminales

---
+++ src/main/java/controllers/ConsultasController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class ConsultasController {
+	
+	public ModelAndView historicoConsultas(Request req, Response res){
+		
+		return null;
+	}
+
+}
---
+++ src/main/java/controllers/TerminalesController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+
+public class TerminalesController {
+	
+	public ModelAndView administrarTerminales(Request req, Response res){
+		return null;
+	}
+
+}
