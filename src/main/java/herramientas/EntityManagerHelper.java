4cd41139fa919ba3f8d5d8fa74d1404051b3956e
Marco Di Croce <marcodicro@hotmail.com>
2016-11-14 15:24:53 -0300

    fix merge in EntityManagerHelper

--- src/main/java/herramientas/EntityManagerHelper.java
+++ src/main/java/herramientas/EntityManagerHelper.java
@@ -1,5 +1,6 @@
 package herramientas;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.*;
 
@@ -9,74 +10,76 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 
+import pois.Banco;
 import pois.Comercio;
+import pois.Direccion;
 import pois.Dispositivo;
 import pois.POI;
-
+import pois.Posicion;
 
 public class EntityManagerHelper {
 
-    private static EntityManagerFactory emf;
+	private static EntityManagerFactory emf;
 
-    private static ThreadLocal<EntityManager> threadLocal;
+	private static ThreadLocal<EntityManager> threadLocal;
 
-    static {
-    	try {
+	static {
+		try {
 			emf = Persistence.createEntityManagerFactory("db");
 			threadLocal = new ThreadLocal<>();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-    
-    public static EntityManager entityManager() {
-    	return getEntityManager();
-    }
-    
-    public static EntityManager getEntityManager() {
+	}
+
+	public static EntityManager entityManager() {
+		return getEntityManager();
+	}
+
+	public static EntityManager getEntityManager() {
 		EntityManager manager = threadLocal.get();
 		if (manager == null || !manager.isOpen()) {
-		    manager = emf.createEntityManager();
-		    threadLocal.set(manager);
+			manager = emf.createEntityManager();
+			threadLocal.set(manager);
 		}
 		return manager;
-    }
+	}
 
-    public static void closeEntityManager() {
+	public static void closeEntityManager() {
 		EntityManager em = threadLocal.get();
 		threadLocal.set(null);
 		em.close();
-    }
+	}
 
-    public static void beginTransaction() {
-    	EntityManager em = getEntityManager();
+	public static void beginTransaction() {
+		EntityManager em = getEntityManager();
 		EntityTransaction tx = em.getTransaction();
-		
-		if(!tx.isActive()){
+
+		if (!tx.isActive()) {
 			tx.begin();
 		}
-    }
+	}
 
-    public static void commit() {
-    	EntityManager em = getEntityManager();
+	public static void commit() {
+		EntityManager em = getEntityManager();
 		EntityTransaction tx = em.getTransaction();
-		if(tx.isActive()){
+		if (tx.isActive()) {
 			tx.commit();
 		}
 
-    }
+	}
 
-    public static void rollback(){
-    	EntityManager em = getEntityManager();
+	public static void rollback() {
+		EntityManager em = getEntityManager();
 		EntityTransaction tx = em.getTransaction();
-		if(tx.isActive()){
+		if (tx.isActive()) {
 			tx.rollback();
 		}
-    }
-    
-    public static Query createQuery(String query) {
-    	return getEntityManager().createQuery(query);
-    }
+	}
+
+	public static Query createQuery(String query) {
+		return getEntityManager().createQuery(query);
+	}
 
 	public static void withTransaction(Runnable action) {
 		withTransaction(() -> {
@@ -84,17 +87,18 @@ public class EntityManagerHelper {
 			return null;
 		});
 	}
-    public static <A> A withTransaction(Supplier<A> action) {
-    	beginTransaction();
-    	try {
-    		A result = action.get();
-    		commit();
+
+	public static <A> A withTransaction(Supplier<A> action) {
+		beginTransaction();
+		try {
+			A result = action.get();
+			commit();
 			return result;
-    	} catch(Throwable e) {
-    		rollback();
-    		throw e;
-    	}
-    }
+		} catch (Throwable e) {
+			rollback();
+			throw e;
+		}
+	}
 
 	public static void persistir(Object object) {
 		beginTransaction();
@@ -102,9 +106,9 @@ public class EntityManagerHelper {
 		commit();
 	}
 
-	 public static <T> T find(Class<T> entityClass, Object primaryKey){
-		 return entityManager().find(entityClass, primaryKey);
-	 }
+	public static <T> T find(Class<T> entityClass, Object primaryKey) {
+		return entityManager().find(entityClass, primaryKey);
+	}
 
 	public static void flush() {
 		entityManager().flush();
@@ -124,40 +128,55 @@ public class EntityManagerHelper {
 
 	@SuppressWarnings("unchecked")
 	public static List<POI> traerTodosLosPOIs() {
+		/*
+		List<String> etiquetas = new ArrayList<String>(){{
+			add("banco");
+			add("comafi");
+			add("tarjeta");
+		}};
+		
+		Direccion direccion = new Direccion("Rosario", 72, "Av La Plata", "Senillosa", null, null, 1424, "CABA",
+				"Caballito", "CABA", "Argentina");
+		
+		POI poi = new Banco(new Posicion(12.50,25.55), "Banco Comafi Caballito", direccion, etiquetas);
+		persistir(poi);
+		*/
+		
 		return entityManager().createQuery("from POI").getResultList();
 	}
 
 	public static void actualizarPoi(POI poiNuevo, Long poiViejoID) {
+
 		beginTransaction();
 		POI poiViejo = find(POI.class, poiViejoID);
 		poiViejo.copiarEstado(poiNuevo);
 		flush();
 		commit();
-		
+
 	}
 
 	public static void eliminarPoi(POI poi) {
 		beginTransaction();
 		POI poiEncontrado = find(POI.class, poi.getId());
 		remover(poiEncontrado);
-		commit();		
+		commit();
 	}
 
-	public static Integer actualizarEtiquetasComerciosYRetornarCantidadModificados(String nombre, List<String> etiquetas) {
+	public static Integer actualizarEtiquetasComerciosYRetornarCantidadModificados(String nombre,
+			List<String> etiquetas) {
 		beginTransaction();
 		List<Comercio> comercios = buscarComercios(nombre);
 		comercios.stream().forEach(comercio -> comercio.setEtiquetas(etiquetas));
 
 		flush();
 		commit();
-		
+
 		return comercios.size();
 	}
 
 	@SuppressWarnings("unchecked")
 	public static List<Comercio> buscarComercios(String nombre) {
-		return createQuery("from Comercio where nombre = :nombre")
-				.setParameter("nombre", nombre).getResultList();
+		return createQuery("from Comercio where nombre = :nombre").setParameter("nombre", nombre).getResultList();
 
 	}
 
@@ -202,7 +221,5 @@ public class EntityManagerHelper {
 	public static void detach(Object entity) {
 		entityManager().detach(entity);
 	}
-    
-    
 
 }
