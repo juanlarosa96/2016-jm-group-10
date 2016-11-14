082f5299d75779a5294c48e4ef4bb184ec9125f2
Marco Di Croce <marcodicro@hotmail.com>
2016-11-13 22:46:04 -0300

    fix administrar pois en pois controller

--- src/main/java/controllers/PoisController.java
+++ src/main/java/controllers/PoisController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
@@ -17,44 +18,60 @@ import spark.Response;
 public class PoisController implements WithGlobalEntityManager, TransactionalOps {
 
 	public ModelAndView buscarPois(Request req, Response res) {
-		
-		String descripcion = req.queryParams("descripcion");		
-		
+
+		String descripcion = req.queryParams("descripcion");
+
 		List<POI> pois = ManejadorDePois.getInstance().buscarPOIs(descripcion);
-		
+
 		Map<String, List<POI>> model = new HashMap<>();
-		
-		model.put("pois", pois);			
-				
+
+		model.put("pois", pois);
+
 		return new ModelAndView(model, "terminal/buscarPois.hbs");
 	}
-	
-public ModelAndView administrarPois(Request req, Response res) {
-		
-		Map<String, List<String>> model = new HashMap<>();
-		
-		List<String> tipos = new ArrayList<String>();
-		
-		tipos.add("Banco");
-		tipos.add("CGP");
-		tipos.add("Comercio");		
-		tipos.add("Parada Colectivo");
-		
-		model.put("tipos",tipos);
-				
+
+	public ModelAndView administrarPois(Request req, Response res) {
+
+		Map<String, List<POI>> model = new HashMap<>();
+
+		String descripcion = req.queryParams("descripcion");
+		String tipo = req.queryParams("tipo");
+
+		List<POI> pois;
+
+		if (descripcion != null && !descripcion.equals(""))
+			pois = ManejadorDePois.getInstance().buscarPOIs(descripcion);
+
+		else
+			pois = ManejadorDePois.getInstance().traerTodosLosPois();
+
+		if (tipo != null && !tipo.equals("Todos"))
+			pois = pois.stream().filter(poi -> poi.getTipo().equals(tipo)).collect(Collectors.toList());
+
+		model.put("pois", pois);
+
 		return new ModelAndView(model, "admin/administrarPois.hbs");
 	}
 
-public ModelAndView mostrarPOI(Request req, Response res){
-	Map<String, POI> model = new HashMap<>();
-	String id = req.params("id");
-	
-	POI poi = ManejadorDePois.getInstance().getPOI(Long.parseLong(id));
-	
-	model.put("poi", poi);
-	
-	return new ModelAndView(model, "pois/mostrarPOI.hbs");
-}
+	public ModelAndView mostrarPOI(Request req, Response res) {
+		Map<String, POI> model = new HashMap<>();
+		String id = req.params("id");
 
+		POI poi = ManejadorDePois.getInstance().getPOI(Long.parseLong(id));
+
+		model.put("poi", poi);
+
+		return new ModelAndView(model, "pois/mostrarPOI.hbs");
+	}
+
+	public ModelAndView editarPOI(Request req, Response res) {
+
+		return null;
+	}
+
+	public ModelAndView borrarPOI(Request req, Response res) {
+
+		return null;
+	}
 
 }
