2d30d927ee4b70da798479954935106350541ed6
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-11-14 18:16:31 -0300

    Agrego agregarTerminal al Router

--- src/main/java/server/Router.java
+++ src/main/java/server/Router.java
@@ -36,6 +36,7 @@ public class Router {
 		Spark.get("/admin/administrarTerminales/", terminalesController::administrarTerminales, engine);
 //		Spark.get("/admin/administrarTerminales/acciones/:id", terminalesController::mostrarAcciones, engine);
 //		Spark.get("/admin/administrarTerminales/editar/:id", terminalesController::editarTerminal, engine);
+		Spark.get("/admin/administrarTerminales/agregar/", terminalesController::agregarTerminal, engine);
 		Spark.get("/admin/administrarTerminales/eliminar/:id", terminalesController::eliminarTerminal, engine);
 		Spark.get("/admin/historicoConsultas/", consultasController::historicoConsultas, engine);
 		Spark.get("/admin/administrarPois/editarPOI/:id", poisController::editarPOI, engine);
