516b960bdeebe6df85e6ebae223fa2e1046737df
Marcos Caccavaio <marcoseze95@hotmail.com>
2016-11-14 18:17:21 -0300

    Agrego metodo de agregarTerminal, falta que se cree la misma con los datos ingresados

--- src/main/java/controllers/TerminalesController.java
+++ src/main/java/controllers/TerminalesController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +8,6 @@ import herramientas.EntityManagerHelper;
 import pois.Dispositivo;
 import pois.ExceptionComunaInvalida;
 import pois.ManejadorDeDispositivos;
-import pois.Posicion;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -17,36 +15,35 @@ import spark.Response;
 public class TerminalesController {
 
 	public ModelAndView administrarTerminales(Request req, Response res) throws Exception, ExceptionComunaInvalida {
+		Map<String, List<Dispositivo>> model = new HashMap<>();
+		
 		String comuna = req.queryParams("comuna");
-		List<Dispositivo> terminales = new ArrayList<Dispositivo>();
+		List<Dispositivo> terminales;
 
 		if (comuna == null || comuna.equals("Todas")) {
 			terminales = ManejadorDeDispositivos.getInstance().getListaDispositivos();
-			Dispositivo dispositivo = new Dispositivo("Terminal 1", new Posicion(-34.622382, -58.511592));
-			terminales.add(dispositivo);
-			EntityManagerHelper.persistir(dispositivo);
+			
 		} else {
 			terminales = ManejadorDeDispositivos.getInstance().filtrarPorComuna(Integer.parseInt(comuna));
 		}
-
-		Map<String, List<Dispositivo>> model = new HashMap<>();
 
 		model.put("terminales", terminales);
 		return new ModelAndView(model, "admin/administrarTerminales.hbs");
 	}
 
-	public ModelAndView eliminarTerminal(Request req, Response res) {
+	public ModelAndView eliminarTerminal(Request req, Response res) throws ExceptionComunaInvalida, Exception {
 		Integer id = Integer.parseInt(req.params("id"));
 		Dispositivo dispositivo = EntityManagerHelper.find(Dispositivo.class, id);
 
 		ManejadorDeDispositivos.getInstance().eliminarDispositivo(dispositivo);
-		
-		List<Dispositivo> terminales = new ArrayList<>();
-		terminales = ManejadorDeDispositivos.getInstance().getListaDispositivos();
-		Map<String, List<Dispositivo>> model = new HashMap<>();
 
-		model.put("terminales", terminales);
-		return new ModelAndView(model, "admin/administrarTerminales.hbs");
+		return this.administrarTerminales(req, res);
+	}
+	
+	public ModelAndView agregarTerminal(Request req, Response res) {
+		Map<String, String> model = new HashMap<>();
+
+		return new ModelAndView(model, "terminal/agregarTerminal.hbs");
 	}
 
 }
