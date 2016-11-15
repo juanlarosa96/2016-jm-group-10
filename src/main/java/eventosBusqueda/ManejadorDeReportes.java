e0e800479501a93999a5997aaae35ccc7989b8e2
Marco Di Croce <marcodicro@hotmail.com>
2016-11-14 23:49:15 -0300

    rename method buscarTodosLosResultadosBusqueda

--- src/main/java/eventosBusqueda/ManejadorDeReportes.java
+++ src/main/java/eventosBusqueda/ManejadorDeReportes.java
@@ -43,7 +43,7 @@ public class ManejadorDeReportes extends InteresadoEnBusquedas {
 	}
 
 	private List<ResultadoBusqueda> busquedasPersistidas() {
-		return persistidor.buscarTodosLosResultadosBusqueda();
+		return persistidor.obtenerTodosLosResultadosBusqueda();
 	}
 
 	@Override
