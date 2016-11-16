2e58696eb1a372b6962c2a9e585e2bb60e8d67e8
Marco Di Croce <marcodicro@hotmail.com>
2016-11-16 16:48:32 -0300

    elimino test que compara paradas colectivo por posicion. ahora importa tambien el id

--- src/test/java/tests/ParadaColectivoTest.java
+++ src/test/java/tests/ParadaColectivoTest.java
@@ -10,23 +10,23 @@ import fixtures.FixtureParadaColectivo;
 import pois.ParadaColectivo;
 
 public class ParadaColectivoTest {
-	private DateTime unHorarioCualquiera;
-	private ParadaColectivo paradaValida;
-	private ParadaColectivo otraParadaValida;
-	private ParadaColectivo otraParadaConIgualPosicionDeParadaValida;
-	private Posicion posicionLejanaParadaValida;
-	private Posicion posicionCercanaParadaValida;
-	
+	private  DateTime unHorarioCualquiera;
+	private  ParadaColectivo paradaValida;
+	private  ParadaColectivo otraParadaConIgualPosicionDeParadaValida;
+	private  Posicion posicionLejanaParadaValida;
+	private  Posicion posicionCercanaParadaValida;
+
 	@Before
-	public void init() {		
-		unHorarioCualquiera=FixtureParadaColectivo.dameUnHorarioCualquiera();
-		paradaValida=FixtureParadaColectivo.dameUnaParadaValida();
+	public void beforeClass() {
+		unHorarioCualquiera = FixtureParadaColectivo.dameUnHorarioCualquiera();
+
+		paradaValida = FixtureParadaColectivo.dameUnaParadaValida();
+		otraParadaConIgualPosicionDeParadaValida = FixtureParadaColectivo
+				.dameOtraParadaConIgualPosicionQueParadaValida();
+
 		posicionLejanaParadaValida = FixtureParadaColectivo.dameUnaPosicionLejanaParadaValida();
 		posicionCercanaParadaValida = FixtureParadaColectivo.dameUnaPosicionCercanaParadaValida();
-		otraParadaValida = FixtureParadaColectivo.dameOtraParadaValida();
-		otraParadaConIgualPosicionDeParadaValida = FixtureParadaColectivo.dameOtraParadaConIgualPosicionQueParadaValida();
 	}
-	
 
 	@Test
 	public void SiPreguntoSiUnaParadaEstaDisponibleEnUnHorarioCualquieraRespondeTrue() {
@@ -37,21 +37,15 @@ public class ParadaColectivoTest {
 	public void SiPreguntoSiUnaParadaEstaCercaDeUnaPosicionLejanaAEllaRespondeFalse() {
 		Assert.assertFalse(paradaValida.estasCerca(posicionLejanaParadaValida));
 	}
+
 	@Test
-	public void SiPreguntoSiUnaParadaEstaCercaDeUnaPosicionCercanaAEllaRespondeTrue(){
+	public void SiPreguntoSiUnaParadaEstaCercaDeUnaPosicionCercanaAEllaRespondeTrue() {
 		Assert.assertTrue(paradaValida.estasCerca(posicionCercanaParadaValida));
 	}
-	
+
 	@Test
-	public void SiPreguntoSiUnaParadaEsIgualAOtraParadaConDistintaPosicionRespondeFalse(){
-		Assert.assertFalse(paradaValida.esIgualA(otraParadaValida));
-	}
-	
-	@Test
-	public void SiPreguntoSiUnaParadaEsIgualAOtraParadaConIgualPosicionRespondeTrue(){
+	public void SiPreguntoSiUnaParadaEsIgualAOtraParadaConIgualPosicionRespondeTrue() {
 		Assert.assertTrue(paradaValida.esIgualA(otraParadaConIgualPosicionDeParadaValida));
 	}
-	
 
-    
 }
